0190ebca8687bbea40c581139959fd5ae1acdcba
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-12-22 16:25:08 -0800

    Models use kwargs only

--- src/vanilla/Model/CodeModelPy.cs
+++ src/vanilla/Model/CodeModelPy.cs
@@ -142,7 +142,7 @@ namespace AutoRest.Python.Model
 
         public virtual string UserAgent => PackageName;
 
-        public virtual string SetupRequires => @"""msrest>=0.2.0""";
+        public virtual string SetupRequires => @"""msrest>=0.4.22""";
         
 
         public string Version => Settings.Instance.PackageVersion.Else(ApiVersion);
