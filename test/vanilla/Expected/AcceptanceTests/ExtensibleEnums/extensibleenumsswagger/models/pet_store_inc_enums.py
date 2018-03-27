769636e2d9c0eb742e485699202a26208c71a8c5
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-03-27 11:22:49 -0700

    Add doc to enum

--- test/vanilla/Expected/AcceptanceTests/ExtensibleEnums/extensibleenumsswagger/models/pet_store_inc_enums.py
+++ test/vanilla/Expected/AcceptanceTests/ExtensibleEnums/extensibleenumsswagger/models/pet_store_inc_enums.py
@@ -25,6 +25,6 @@ class DaysOfWeekExtensibleEnum(str, Enum):
 
 class IntEnum(str, Enum):
 
-    one = "1"
-    two = "2"
-    three = "3"
+    one = "1"  #: one
+    two = "2"  #: two
+    three = "3"  #: three
