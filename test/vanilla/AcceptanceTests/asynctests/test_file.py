31cb8838a0f5763ec48f1c8496e2ddad3eedb3d8
antisch <antisch@microsoft.com>
2019-05-02 15:14:05 -0700

    Updated async file tests

--- test/vanilla/AcceptanceTests/asynctests/test_file.py
+++ test/vanilla/AcceptanceTests/asynctests/test_file.py
@@ -65,7 +65,7 @@ class TestFile(object):
             with io.BytesIO() as file_handle:
                 stream = await client.files.get_file()
                 total = len(stream)
-                assert not stream.response.closed
+                assert not stream.response._released
 
                 async for data in stream:
                     assert 0 < len(data) <= stream.block_size
@@ -73,10 +73,11 @@ class TestFile(object):
                     print("Downloading... {}%".format(int(file_length*100/total)))
                     file_handle.write(data)
 
+                assert stream.response._released
                 assert file_length !=  0
 
                 sample_file = realpath(
-                    join(cwd, pardir, pardir, pardir,
+                    join(cwd, pardir, pardir, pardir, pardir,
                         "node_modules", "@microsoft.azure", "autorest.testserver", "routes", "sample.png"))
 
                 with open(sample_file, 'rb') as data:
@@ -90,113 +91,66 @@ class TestFile(object):
             with io.BytesIO() as file_handle:
                 stream = await client.files.get_empty_file()
                 assert len(stream) == 0
-                assert not stream.response.closed
+                assert not stream.response._released
 
-                for data in stream:
+                async for data in stream:
                     file_length += len(data)
                     file_handle.write(data)
 
+                assert stream.response._released
                 assert file_length ==  0
 
-    
+    @pytest.mark.asyncio
+    async def test_files_long_running(self):
+        pytest.skip("slow")
+        config = AutoRestSwaggerBATFileServiceConfiguration()
+        config.connection.data_block_size = 4096
+        async with AutoRestSwaggerBATFileService(base_url="http://localhost:3000", config=config) as client:
+            file_length = 0
+            stream = await client.files.get_file_large()
+            async for data in stream:
+                assert 0 < len(data) <= stream.block_size
+                file_length += len(data)
 
-    # @pytest.mark.asyncio
-    # async def test_files(self, client):
-    #     config = AutoRestSwaggerBATFileServiceConfiguration()
-    #     config.connection.data_block_size = 1000
+        assert file_length ==  3000 * 1024 * 1024
 
-    #     client = AutoRestSwaggerBATFileService(base_url="http://localhost:3000", config=config)
+    @pytest.mark.asyncio
+    async def test_files_raw(self, client):
 
-    #     def test_callback(data, response, progress=[0]):
-    #         assert len(data) > 0
-    #         assert response is not None
-    #         assert not response._content_consumed
-    #         total = float(response.headers['Content-Length'])
-    #         if total < 4096:
-    #             progress[0] += len(data)
-    #             print("Downloading... {}%".format(int(progress[0]*100/total)))
+        def test_callback(response, data_stream, headers):
+            assert not data_stream.response._released
+            return data_stream
 
-    #     file_length = 0
-    #     with io.BytesIO() as file_handle:
-    #         stream = await client.files.get_file(callback=test_callback)
+        file_length = 0
+        with io.BytesIO() as file_handle:
+            stream = await client.files.get_file(cls=test_callback)
+            assert len(stream) > 0
+            async for data in stream:
+                assert 0 < len(data) <= stream.block_size
+                file_length += len(data)
+                file_handle.write(data)
 
-    #         async for data in stream:
-    #             file_length += len(data)
-    #             file_handle.write(data)
+            assert stream.response._released
+            assert file_length !=  0
 
-    #         assert file_length !=  0
+            sample_file = realpath(
+                join(cwd, pardir, pardir, pardir, pardir,
+                     "node_modules", "@microsoft.azure", "autorest.testserver", "routes", "sample.png"))
 
-    #         sample_file = realpath(
-    #             join(cwd, pardir, pardir, pardir, pardir,
-    #                  "node_modules", "@microsoft.azure", "autorest.testserver", "routes", "sample.png"))
+            with open(sample_file, 'rb') as data:
+                sample_data = hash(data.read())
+            assert sample_data ==  hash(file_handle.getvalue())
 
-    #         with open(sample_file, 'rb') as data:
-    #             sample_data = hash(data.read())
-    #         assert sample_data ==  hash(file_handle.getvalue())
+        file_length = 0
+        with io.BytesIO() as file_handle:
+            stream = await client.files.get_empty_file(cls=test_callback)
+            async for data in stream:
+                file_length += len(data)
+                file_handle.write(data)
 
-    #     client.config.connection.data_block_size = 4096
-    #     file_length = 0
-    #     with io.BytesIO() as file_handle:
-    #         stream = await client.files.get_empty_file(callback=test_callback)
+            assert stream.response._released
+            assert file_length ==  0
 
-    #         async for data in stream:
-    #             file_length += len(data)
-    #             file_handle.write(data)
-
-    #         assert file_length ==  0
-
-    #     def add_headers(adapter, request, response, *args, **kwargs):
-    #         response.headers['Content-Length'] = str(3000 * 1024 * 1024)
-
-    #     file_length = 0
-    #     # client._client.add_hook('response', add_headers)
-    #     stream = await client.files.get_file_large(callback=test_callback)
-    #     #for data in stream:
-    #     #    file_length += len(data)
-
-    #     #assert file_length ==  3000 * 1024 * 1024
-
-    # @pytest.mark.asyncio
-    # async def test_files_raw(self, client):
-
-    #     def test_callback(data, response, progress=[0]):
-    #         assert len(data) > 0
-    #         assert response is not None
-    #         assert not response._content_consumed
-    #         total = float(response.headers.get('Content-Length', 0))
-    #         if total:
-    #             progress[0] += len(data)
-    #             print("Downloading... {}%".format(int(progress[0]*100/total)))
-
-    #     file_length = 0
-    #     with io.BytesIO() as file_handle:
-    #         response = await client.files.get_file(raw=True, callback=test_callback)
-    #         stream = response.output
-
-    #         async for data in stream:
-    #             file_length += len(data)
-    #             file_handle.write(data)
-
-    #         assert file_length !=  0
-
-    #         sample_file = realpath(
-    #             join(cwd, pardir, pardir, pardir, pardir,
-    #                  "node_modules", "@microsoft.azure", "autorest.testserver", "routes", "sample.png"))
-
-    #         with open(sample_file, 'rb') as data:
-    #             sample_data = hash(data.read())
-    #         assert sample_data ==  hash(file_handle.getvalue())
-
-    #     file_length = 0
-    #     with io.BytesIO() as file_handle:
-    #         response = await client.files.get_empty_file(raw=True, callback=test_callback)
-    #         stream = response.output
-
-    #         async for data in stream:
-    #             file_length += len(data)
-    #             file_handle.write(data)
-
-    #         assert file_length ==  0
 
 if __name__ == '__main__':
     unittest.main()
