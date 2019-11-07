7c1150bc36d867aaa5a0a49ba6c67f9ab06e1801
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-11-07 14:51:52 -0800

    No fail if modeler 4.0.31

--- autorest/common/utils.py
+++ autorest/common/utils.py
@@ -1,5 +1,11 @@
+import logging
+
 from .code_namer import CodeNamer
 from .known_primary_types_mapping import known_primary_types_mapping
+
+
+_LOGGER = logging.getLogger(__name__)
+
 
 def get_property_name(name):
         return CodeNamer().get_valid_python_name(name, "Property")
@@ -20,8 +26,8 @@ def to_python_type(original_type):
     try:
         return known_primary_types_mapping[original_type]
     except KeyError as e:
-        print("The type you are trying to convert to Python is not a known type")
-        raise
+        _LOGGER.critical("The type you are trying to convert to Python is not a known type")
+        return "KEYERROR" # FIXME
     except:
         # one entry in the swagger has three types under type[]
         # https://github.com/Azure/perks/blob/master/codemodel/.resources/all-in-one/json/code-model.json#L882
