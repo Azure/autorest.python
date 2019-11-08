7067abacc2ad207e996884f672c845df9f30f6fd
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-11-08 15:07:44 -0800

    Fix number serialisation type

--- autorest/models/primitive_schemas.py
+++ autorest/models/primitive_schemas.py
@@ -78,6 +78,14 @@ class NumberSchema(PrimitiveSchema):
             exclusive_minimum=yaml_data.get('exclusiveMinimum'),
         )
 
+    def get_serialization_type(self):
+        if self.yaml_data['type'] == "integer":
+            if self.precision == 64:
+                return "long"
+            else:
+                return "int"
+        return "float"
+
 class StringSchema(PrimitiveSchema):
     def __init__(self, yaml_data, name, schema_type, **kwargs):
         super(StringSchema, self).__init__(yaml_data, name, schema_type, **kwargs)
@@ -123,6 +131,27 @@ class DatetimeSchema(PrimitiveSchema):
         )
 
 
+class DateSchema(PrimitiveSchema):
+    def __init__(self, yaml_data, name, schema_type, **kwargs):
+        super(DateSchema, self).__init__(yaml_data, name, schema_type, **kwargs)
+        self.format = format
+
+    def get_serialization_type(self):
+        return "date"
+
+    def get_doc_string_type(self, namespace=None):
+        return "~datetime.date"
+
+    @classmethod
+    def from_yaml(cls, name, yaml_data, schema_type, original_swagger_name):
+        return cls(
+            yaml_data=yaml_data,
+            name=name,
+            schema_type=schema_type,
+            original_swagger_name=original_swagger_name
+        )
+
+
 class ByteArraySchema(PrimitiveSchema):
     def __init__(self, yaml_data, name, schema_type, format, **kwargs):
         super(ByteArraySchema, self).__init__(yaml_data, name, schema_type, **kwargs)
@@ -161,6 +190,13 @@ def get_primitive_schema(name, yaml_data):
             name=name,
             yaml_data=yaml_data,
             schema_type=schema_type
+        )
+    if schema_type == 'date':
+        return DateSchema.from_yaml(
+            name=name,
+            yaml_data=yaml_data,
+            schema_type=schema_type,
+            original_swagger_name=original_swagger_name
         )
     if schema_type  == 'byte-array':
         return ByteArraySchema.from_yaml(
