af4ede6370b2e4c5c5f1e53e65951ba091905661
Isabella Cai <iscai@microsoft.com>
2019-12-02 10:58:45 -0800

    changed imports for async operations

--- autorest/serializers/operation_group_serializer.py
+++ autorest/serializers/operation_group_serializer.py
@@ -1,4 +1,4 @@
-from .import_serializer import FileImportSerializer
+from .import_serializer import FileImportSerializer, ImportType
 
 class OperationGroupSerializer:
     def __init__(self, code_model, env, operation_group, async_mode):
@@ -8,13 +8,34 @@ class OperationGroupSerializer:
         self.async_mode = async_mode
         self._operation_group_file = None
 
+    @staticmethod
+    def _change_imports_for_async(imports):
+        # change model import for async operation groups
+        local_imports = imports.imports[ImportType.LOCAL]
+        if local_imports.get("..."):
+            local_imports["..."].add("models")
+        else:
+            local_imports["..."] = {"models"}
+        local_imports[".."].remove("models")
+        if not local_imports[".."]:
+            del local_imports[".."]
+
+        # change distributed_trace import for async operation groups
+        azure_core_imports = imports.imports[ImportType.AZURECORE]
+        if azure_core_imports.get("azure.core.tracing.decorator"):
+            azure_core_imports["azure.core.tracing.decorator_async"] = {"distributed_trace_async"}
+            del azure_core_imports["azure.core.tracing.decorator"]
+
+
     def serialize(self):
         operation_group_template = self.env.get_template("operations_container.py.jinja2")
-
+        imports = self.operation_group.imports()
+        if self.async_mode:
+            OperationGroupSerializer._change_imports_for_async(imports)
         self._operation_group_file = operation_group_template.render(
             code_model=self.code_model,
             operation_group=self.operation_group,
-            imports=FileImportSerializer(self.operation_group.imports()),
+            imports=FileImportSerializer(imports),
             async_mode=self.async_mode
         )
 
