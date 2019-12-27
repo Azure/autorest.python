b383869446334f59fbe2111ef27cb9e6ba83762e
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-12-27 11:25:38 -0800

    Licence header (#266)
    * Licence header
    
    * Update server.py

--- autorest/models/__init__.py
+++ autorest/models/__init__.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 from .code_model import CodeModel
 from .object_schema import ObjectSchema
 from .dictionary_schema import DictionarySchema
--- autorest/models/constant_schema.py
+++ autorest/models/constant_schema.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 import logging
 from .base_schema import BaseSchema
 from .primitive_schemas import get_primitive_schema
