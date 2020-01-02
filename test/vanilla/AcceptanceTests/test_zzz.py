2223ddcb6f1cbb3de9d87ef6f667b19c1297ddba
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-01-02 15:50:12 -0800

    Plug failure on tests missing

--- test/vanilla/AcceptanceTests/test_zzz.py
+++ test/vanilla/AcceptanceTests/test_zzz.py
@@ -23,7 +23,6 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
-
 import unittest
 import isodate
 import subprocess
@@ -32,6 +31,7 @@ import datetime
 import os
 import platform
 from os.path import dirname, pardir, join, realpath
+import warnings
 
 from report import AutoRestReportService
 
@@ -41,6 +41,7 @@ class TestAcceptance(object):
     def test_ensure_coverage(self):
         client = AutoRestReportService(base_url="http://localhost:3000")
         report = client.get_report(platform.python_version())
+        optional_report = client.get_optional_report(platform.python_version())
 
         # Add tests that wont be supported due to the nature of Python here
         not_supported = {}
@@ -48,23 +49,31 @@ class TestAcceptance(object):
         # Please add missing features or failing tests here
         missing_features_or_bugs = {'FileStreamVeryLarge' : 1}
 
-        report.update(not_supported)
-        report.update(missing_features_or_bugs)
+        print("Coverage:")
+        self._print_report(report, not_supported, missing_features_or_bugs)
+        print("Optional coverage:")
+        self._print_report(optional_report)
+
+
+    def _print_report(self, report, not_supported=None, missing_features_or_bugs=None):
+        if not_supported:
+            report.update(not_supported)
+            for s in not_supported.keys():
+                print("IGNORING {0}".format(s))
+
+        if missing_features_or_bugs:
+            report.update(missing_features_or_bugs)
+            for s in missing_features_or_bugs.keys():
+                print("PENDING {0}".format(s))
+
         failed = [k for k, v in report.items() if v == 0]
-
-        for s in not_supported.keys():
-            print("IGNORING {0}".format(s))
-
-        for s in missing_features_or_bugs.keys():
-            print("PENDING {0}".format(s))
-
         for s in failed:
             print("FAILED TO EXECUTE {0}".format(s))
 
-        totalTests = len(report)
-        print ("The test coverage is {0}/{1}.".format(totalTests - len(failed), totalTests))
+        total_tests = len(report)
+        warnings.warn ("The test coverage is {0}/{1}.".format(total_tests - len(failed), total_tests))
 
-        # assert 0 ==  len(failed)
+        assert 0 == len(failed)
 
 if __name__ == '__main__':
     unittest.main()
--- test/vanilla/Expected/AcceptanceTests/Report/report/aio/operations_async/_auto_rest_report_service_operations_async.py
+++ test/vanilla/Expected/AcceptanceTests/Report/report/aio/operations_async/_auto_rest_report_service_operations_async.py
@@ -61,3 +61,50 @@ class AutoRestReportServiceOperationsMixin:
         return deserialized
     get_report.metadata = {'url': '/report'}
 
+    @distributed_trace_async
+    async def get_optional_report(self, qualifier=None, cls=None, **kwargs):
+        """Get optional test coverage report.
+
+        FIXME: add operation.summary
+
+
+        :param qualifier: If specified, qualifies the generated report further (e.g. '2.7' vs '3.5' in for Python). The only effect is, that generators that run all tests several times, can distinguish the generated reports.
+        :type qualifier: str
+        :param callable cls: A custom type or function that will be passed the direct response
+        :return:  or the result of cls(response)
+        :rtype: dict[str, int]
+        :raises: ~report.models.ErrorException:
+        """
+        error_map = kwargs.pop('error_map', None)
+
+        # Construct URL
+        url = self.get_optional_report.metadata['url']
+
+        # Construct parameters
+        query_parameters = {}
+        if qualifier is not None:
+            query_parameters['qualifier'] = self._serialize.query("qualifier", qualifier, 'str')
+
+
+        # Construct headers
+        header_parameters = {}
+        header_parameters['Accept'] = 'application/json'
+
+
+        # Construct and send request
+        request = self._client.get(url, query_parameters, header_parameters)
+        pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            raise models.ErrorException(response, self._deserialize)
+
+        deserialized = self._deserialize('{int}', response)
+
+        if cls:
+          return cls(response, deserialized, {})
+
+        return deserialized
+    get_optional_report.metadata = {'url': '/report/optional'}
+
