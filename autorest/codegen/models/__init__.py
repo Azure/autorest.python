a259fed0f6d932b36fd42032f6afcf9b91a55e0b
iscai-msft <iscai@microsoft.com>
2020-01-13 18:07:53 -0800

    added operation to list of models to import from models module

--- autorest/codegen/models/__init__.py
+++ autorest/codegen/models/__init__.py
@@ -16,6 +16,7 @@ from .imports import FileImport, ImportType
 from .lro_operation import LROOperation
 from .paging_operation import PagingOperation
 from .parameter import Parameter
+from .operation import Operation
 
 
 __all__ = [
@@ -32,6 +33,7 @@ __all__ = [
     "ImportType",
     "PrimitiveSchema",
     "LROOperation",
+    "Operation",
     "PagingOperation",
     "Parameter"
 ]
