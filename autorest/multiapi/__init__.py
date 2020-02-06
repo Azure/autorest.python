503c4a146d4a3903dea48094cf1d1e688630d8af
Isabella Cai <iscai@microsoft.com>
2020-02-05 18:05:44 -0800

    able to get everything into client, going to make client v3 tomorrow

--- autorest/multiapi/__init__.py
+++ autorest/multiapi/__init__.py
@@ -10,9 +10,14 @@ import sys
 import argparse
 import logging
 import azure
+import json
+import shutil
 
+from collections import defaultdict
 from pathlib import Path
-from typing import Any, List, Tuple, Optional
+from typing import Any, Dict, List, Tuple, Optional, Union
+
+from .multiapi_serializer import MultiAPISerializer
 
 try:
     import msrestazure
@@ -75,22 +80,154 @@ class MultiAPI:
         return str(packages[0].relative_to(sdk_root))
 
     @staticmethod
-    def _get_versioned_modules(package_name: str, module_name: str, sdk_root: Optional[Path] = None):
-        """Get (label, submodule) where label starts with "v20" and submodule is the corresponding imported module.
+    def _get_paths_to_versions(path_to_package: str) -> List[str]:
+
+        paths_to_versions = []
+        for child in [x for x in path_to_package.iterdir() if x.is_dir()]:
+            child_dir = (path_to_package / child).resolve()
+            if Path(child_dir / '_metadata.json') in [x for x in child_dir.iterdir()]:
+                paths_to_versions.append(child_dir)
+        return paths_to_versions
+
+    @staticmethod
+    def _get_floating_latest(api_versions_list: List[str], preview_mode: bool):
+        """Get the floating latest, from a random list of API versions.
         """
-        if not sdk_root:
-            sdk_root = Path(__file__).parents[3] / "azure-sdk-for-python"
+        api_versions_list = list(api_versions_list)
+        absolute_latest = sorted(api_versions_list)[-1]
+        trimmed_preview = [
+            version for version in api_versions_list if "preview" not in version
+        ]
 
-        path_to_package = MultiAPI._resolve_package_directory(package_name, sdk_root)
+        # If there is no preview, easy: the absolute latest is the only latest
+        if not trimmed_preview:
+            return absolute_latest
 
-        azure.__path__.append(str((sdk_root / path_to_package / "azure").resolve()))
-        module_to_generate = importlib.import_module(module_name)
-        raise ValueError(module_to_generate)
-        return {
-            label: importlib.import_module("." + label, module_to_generate.__name__)
-            for (_, label, ispkg) in pkgutil.iter_modules(module_to_generate.__path__)
-            if label.startswith("v20") and ispkg
+        # If preview mode, let's use the absolute latest, I don't care preview or stable
+        if preview_mode:
+            return absolute_latest
+
+        # If not preview mode, and there is preview, take the latest known stable
+        return sorted(trimmed_preview)[-1]
+
+    @staticmethod
+    def _build_operation_meta(paths_to_versions: List[Path]):
+        """Introspect the client:
+
+        version_dict => {
+            'application_gateways': [
+                ('v2018_05_01', 'ApplicationGatewaysOperations')
+            ]
         }
+        mod_to_api_version => {'v2018_05_01': '2018-05-01'}
+        """
+        mod_to_api_version = defaultdict(str)
+        versioned_operations_dict = defaultdict(list)
+        for version_path in paths_to_versions:
+            with open(version_path / "_metadata.json") as f:
+                metadata_json = json.load(f)
+            operation_groups = metadata_json['operation_groups']
+            version = metadata_json['version']
+            mod_to_api_version[version_path.name] = version
+            for operation_group, operation_group_class_name in operation_groups.items():
+                versioned_operations_dict[operation_group].append((version_path.name, operation_group_class_name))
+        return versioned_operations_dict, mod_to_api_version
+
+    @staticmethod
+    def _build_operation_mixin_meta(paths_to_versions: List[Path]):
+        """Introspect the client:
+
+        version_dict => {
+            'check_dns_name_availability': {
+                'doc': 'docstring',
+                'signature': '(self, p1, p2, **operation_config),
+                'call': 'p1, p2',
+                'available_apis': [
+                    'v2018_05_01'
+                ]
+            }
+        }
+        """
+        mixin_operations = {}
+        for version_path in paths_to_versions:
+            with open(version_path / "_metadata.json") as f:
+                metadata_json = json.load(f)
+            if not metadata_json.get('operation_mixin_functions'):
+                continue
+            for func_name, func in metadata_json['operation_mixin_functions'].items():
+                if func_name.startswith("_"):
+                    continue
+                mixin_operations.setdefault(func_name, {}).setdefault(
+                    "available_apis", []
+                ).append(version_path.name)
+                mixin_operations[func_name]['doc'] = func['doc']
+                mixin_operations[func_name]['signature'] = func['signature']
+                mixin_operations[func_name][call] = func['call']
+        return mixin_operations
+
+    @staticmethod
+    def _build_last_rt_list(
+        versioned_operations_dict: Dict[str, Tuple[str, str]],
+        mixin_operations: Dict[str, Dict[str, Union[str, List[str]]]],
+        last_api_version: str,
+        preview_mode: bool
+    ):
+        """Build the a mapping RT => API version if RT doesn't exist in latest detected API version.
+
+        Example:
+        last_rt_list = {
+        'check_dns_name_availability': '2018-05-01'
+        }
+
+        There is one subtle scenario if PREVIEW mode is disabled:
+        - RT1 available on 2019-05-01 and 2019-06-01-preview
+        - RT2 available on 2019-06-01-preview
+        - RT3 available on 2019-07-01-preview
+
+        Then, if I put "RT2: 2019-06-01-preview" in the list, this means I have to make
+        "2019-06-01-preview" the default for models loading (otherwise "RT2: 2019-06-01-preview" won't work).
+        But this likely breaks RT1 default operations at "2019-05-01", with default models at "2019-06-01-preview"
+        since "models" are shared for the entire set of operations groups (I wished models would be split by operation groups, but meh, that's not the case)
+
+        So, until we have a smarter Autorest to deal with that, only preview RTs which do not share models with a stable RT can be added to this map.
+        In this case, RT2 is out, RT3 is in.
+        """
+
+        def there_is_a_rt_that_contains_api_version(rt_dict, api_version):
+            "Test in the given api_version is is one of those RT."
+            for rt_api_version in rt_dict.values():
+                if api_version in rt_api_version:
+                    return True
+            return False
+
+        last_rt_list = {}
+
+        # Operation groups
+        versioned_dict = {
+            operation_name: [meta[0] for meta in operation_metadata]
+            for operation_name, operation_metadata in versioned_operations_dict.items()
+        }
+        # Operations at client level
+        versioned_dict.update(
+            {
+                operation_name: operation_metadata["available_apis"]
+                for operation_name, operation_metadata in mixin_operations.items()
+            }
+        )
+        for operation, api_versions_list in versioned_dict.items():
+            local_last_api_version = MultiAPI._get_floating_latest(api_versions_list, preview_mode)
+            if local_last_api_version == last_api_version:
+                continue
+            # If some others RT contains "local_last_api_version", and it's greater than the future default, danger, don't profile it
+            if (
+                there_is_a_rt_that_contains_api_version(
+                    versioned_dict, local_last_api_version
+                )
+                and local_last_api_version > last_api_version
+            ):
+                continue
+            last_rt_list[operation] = local_last_api_version
+        return last_rt_list
 
     def process(self) -> bool:
         # If True, means the auto-profile will consider preview versions.
@@ -101,8 +238,110 @@ class MultiAPI:
         is_multi_client_package = "#" in self.input_str
 
         package_name, module_name = MultiAPI._parse_input(self.input_str)
-        versioned_modules = MultiAPI._get_versioned_modules(package_name, module_name)
-        raise ValueError(versioned_modules)
+        sdk_root = Path(__file__).parents[3] / "azure-sdk-for-python"
+        path_to_package = (
+            sdk_root / MultiAPI._resolve_package_directory(package_name, sdk_root) /
+            Path(*(module_name.split(".")))
+        ).resolve()
+        paths_to_versions = MultiAPI._get_paths_to_versions(path_to_package)
+        versioned_operations_dict, mod_to_api_version = MultiAPI._build_operation_meta(
+            paths_to_versions
+        )
+
+        last_api_version = MultiAPI._get_floating_latest(mod_to_api_version.keys(), preview_mode)
+
+        # I need default_api to be v2019_06_07_preview shaped if it exists, let's be smart
+        # and change it automatically so I can take both syntax as input
+        if self.default_api and not self.default_api.startswith("v"):
+            last_api_version = [
+                mod_api
+                for mod_api, real_api in mod_to_api_version.items()
+                if real_api == self.default_api
+            ][0]
+            _LOGGER.info("Default API version will be: %s", last_api_version)
+
+        last_api_path = path_to_package / last_api_version
+
+        # I need default_api to be v2019_06_07_preview shaped if it exists, let's be smart
+        # and change it automatically so I can take both syntax as input
+        if self.default_api and not self.default_api.startswith("v"):
+            last_api_version = [
+                mod_api
+                for mod_api, real_api in mod_to_api_version.items()
+                if real_api == default_api
+            ][0]
+            _LOGGER.info("Default API version will be: %s", last_api_version)
+
+        last_api_path = path_to_package / last_api_version
+
+        # In case we are transitioning from a single api generation, clean old folders
+        shutil.rmtree(str(path_to_package / "operations"), ignore_errors=True)
+        shutil.rmtree(str(path_to_package / "models"), ignore_errors=True)
+
+        shutil.copy(
+            str(path_to_package / last_api_version / "_configuration.py"),
+            str(path_to_package / "_configuration.py"),
+        )
+        shutil.copy(
+            str(path_to_package / last_api_version / "__init__.py"),
+            str(path_to_package / "__init__.py"),
+        )
+        if is_multi_client_package:
+            _LOGGER.warning("Patching multi-api client basic files")
+            patch_import(path_to_package / "_configuration.py")
+            patch_import(path_to_package / "__init__.py")
+
+        # Detect if this client is using an operation mixin (Network)
+        # Operation mixins are available since Autorest.Python 4.x
+        mixin_operations = MultiAPI._build_operation_mixin_meta(paths_to_versions)
+
+        # get client name from latest api version
+        with open(paths_to_versions[-1] / "_metadata.json") as f:
+            metadata_json = json.load(f)
+
+
+
+
+        # versioned_operations_dict => {
+        #     'application_gateways': [
+        #         ('v2018-05-01', 'ApplicationGatewaysOperations')
+        #     ]
+        # }
+        # mod_to_api_version => {'v2018-05-01': '2018-05-01'}
+        # mixin_operations => {
+        #     'check_dns_name_availability': {
+        #         'doc': 'docstring',
+        #         'signature': '(self, p1, p2, **operation_config),
+        #         'call': 'p1, p2',
+        #         'available_apis': [
+        #             'v2018_05_01'
+        #         ]
+        #     }
+        # }
+        # last_rt_list = {
+        #    'check_dns_name_availability': '2018-05-01'
+        # }
+
+        last_rt_list = MultiAPI._build_last_rt_list(
+            versioned_operations_dict, mixin_operations, last_api_version, preview_mode
+        )
+
+        conf = {
+            "client_name": metadata_json["client"]["name"],
+            "has_subscription_id": metadata_json["client"]["has_subscription_id"],
+            "module_name": module_name,
+            "operations": versioned_operations_dict,
+            "mixin_operations": mixin_operations,
+            "mod_to_api_version": mod_to_api_version,
+            "last_api_version": mod_to_api_version[last_api_version],
+            "client_doc": metadata_json["client"]["description"],
+            "last_rt_list": last_rt_list,
+            "default_models": sorted(
+                {last_api_version} | {versions for _, versions in last_rt_list.items()}
+            ),
+        }
+
+        MultiAPISerializer(conf, path_to_package / metadata_json["client"]["filename"]).serialize()
 
         return True
 
---
+++ autorest/multiapi/multiapi_serializer.py
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from jinja2 import Environment, PackageLoader
+from typing import Dict, List, Union
+
+
+class MultiAPISerializer:
+    def __init__(self, conf, output_filename):
+        self.conf = conf
+        self.output_filename = output_filename
+        self.env = Environment(
+            loader=PackageLoader("autorest.multiapi", "."),
+            keep_trailing_newline=True,
+            line_statement_prefix="##",
+            line_comment_prefix="###",
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+
+    def serialize(self) -> str:
+        template = self.env.get_template("multiapi_service_client.py.jinja2")
+        result = template.render(**self.conf)
+
+        with self.output_filename.open("w") as fd:
+            fd.write(result)
