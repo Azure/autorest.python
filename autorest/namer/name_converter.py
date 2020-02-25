26e6edacb657693085fa663ebc097816c62d5825
Isabella Cai <iscai@microsoft.com>
2020-02-25 09:44:37 -0800

    switched how we discern between enum and object descriptions

--- autorest/namer/name_converter.py
+++ autorest/namer/name_converter.py
@@ -77,13 +77,20 @@ class NameConverter:
 
     @staticmethod
     def _convert_enum_schema(schema):
-        NameConverter._convert_language_default_pascal_case(schema, is_object=False)
+        NameConverter._convert_language_default_pascal_case(schema)
         for choice in schema["choices"]:
             NameConverter._convert_language_default_python_case(choice, pad_string="Enum")
 
     @staticmethod
     def _convert_object_schema(schema):
-        NameConverter._convert_language_default_pascal_case(schema, is_object=True)
+        NameConverter._convert_language_default_pascal_case(schema)
+        schema_description = schema["language"]["python"]["description"]
+        if not schema_description:
+            # what is being used for empty ObjectSchema descriptions
+            schema_description = schema["language"]["python"]["name"]
+        if schema_description and schema_description[-1] != ".":
+            schema_description += "."
+        schema["language"]["python"]["description"] = schema_description
         for prop in schema.get("properties", []):
             NameConverter._convert_language_default_python_case(schema=prop, pad_string="Property")
 
@@ -113,19 +120,14 @@ class NameConverter:
             schema["language"]["python"]["summary"] = schema_summary
 
     @staticmethod
-    def _convert_language_default_pascal_case(schema, is_object):
+    def _convert_language_default_pascal_case(schema):
         if schema["language"].get("python"):
             return
         schema['language']['python'] = dict(schema['language']['default'])
 
         schema_description = schema["language"]["default"]["description"].strip()
-        if is_object:
-            if not schema_description:
-                # what is being used for empty ObjectSchema descriptions
-                schema_description = schema["language"]["python"]["name"]
-            if schema_description and schema_description[-1] != ".":
-                schema_description += "."
-            schema["language"]["python"]["description"] = schema_description
+
+        schema["language"]["python"]["description"] = schema_description
 
     @staticmethod
     def _to_pascal_case(name):
