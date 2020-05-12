ae69fc561cf314ff6d2786516c319d86216afb15
iscai-msft <iscai@microsoft.com>
2020-05-12 11:59:44 -0400

    added unittest

--- test/unittests/test_name_converter.py
+++ test/unittests/test_name_converter.py
@@ -1,7 +1,7 @@
 from autorest.namer.name_converter import NameConverter
 from autorest.namer.python_mappings import PadType
 
-def test_escaped_reserved_words():
+def test_escaped_reserved_words_model():
     expected_conversion_model = {
         "self": "self_model",
         "and": "and_model"
@@ -9,26 +9,31 @@ def test_escaped_reserved_words():
     for name in expected_conversion_model:
         assert NameConverter._to_valid_python_name(name, pad_string=PadType.Model) == expected_conversion_model[name]
 
-
+def test_escaped_reserved_words_method():
     expected_conversion_method = {
         "self": "self_method",
         "and": "and_method",
-        "content_type": "content_type"
+        "content_type": "content_type",
     }
     for name in expected_conversion_method:
         assert NameConverter._to_valid_python_name(name, pad_string=PadType.Method) == expected_conversion_method[name]
 
+def test_escaped_reserved_words_parameter():
     expected_conversion_parameter = {
         "content_type": "content_type_parameter",
         "request_id": "request_id_parameter",
-        "elif": "elif_parameter"
+        "elif": "elif_parameter",
+        "auth": "auth_parameter",
+        "passthrough": "passthrough_parameter"
     }
     for name in expected_conversion_parameter:
         assert NameConverter._to_valid_python_name(name, pad_string=PadType.Parameter) == expected_conversion_parameter[name]
 
+def test_escaped_reserved_words_enum():
     expected_conversion_enum = {
         "self": "self",
-        "mro": "mro_enum"
+        "mro": "mro_enum",
+        "passthrough": "passthrough"
     }
     for name in expected_conversion_enum:
         assert NameConverter._to_valid_python_name(name, pad_string=PadType.Enum) == expected_conversion_enum[name]
