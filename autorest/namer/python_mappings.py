599c8d74a255a40d6aa9a3aaec00d8f639d707c2
iscai-msft <iscai@microsoft.com>
2020-05-14 17:32:31 -0400

    remove 'self' as reserved word for models and methods

--- autorest/namer/python_mappings.py
+++ autorest/namer/python_mappings.py
@@ -94,7 +94,6 @@ _always_reserved = [
 
 reserved_words = {
     PadType.Method: [
-        "self",
         *_always_reserved
     ],
     PadType.Parameter: [
@@ -161,7 +160,6 @@ reserved_words = {
         *_always_reserved
     ],
     PadType.Model: [
-        "self",
         *_always_reserved
     ],
     PadType.Property: [
--- test/unittests/test_name_converter.py
+++ test/unittests/test_name_converter.py
@@ -3,7 +3,7 @@ from autorest.namer.python_mappings import PadType
 
 def test_escaped_reserved_words():
     expected_conversion_model = {
-        "self": "self_model",
+        "self": "self",
         "and": "and_model"
     }
     for name in expected_conversion_model:
@@ -11,7 +11,7 @@ def test_escaped_reserved_words():
 
 
     expected_conversion_method = {
-        "self": "self_method",
+        "self": "self",
         "and": "and_method",
         "content_type": "content_type"
     }
