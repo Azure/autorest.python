2c273ba8e9f4c101a3c4ac029ad51132600b4748
iscai-msft <iscai@microsoft.com>
2020-06-10 17:54:27 -0400

    add credential default policy for multiapi

---
+++ test/multiapi/AcceptanceTests/test_multiapi_credential_default_policy.py
@@ -0,0 +1,41 @@
+# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# --------------------------------------------------------------------------
+import pytest
+from azure.core.pipeline.policies import AzureKeyCredentialPolicy
+
+@pytest.fixture
+def default_client(authentication_policy):
+    from multiapicredentialdefaultpolicy import MultiapiServiceClient
+    with MultiapiServiceClient(
+		base_url="http://localhost:3000",
+        credential="12345",
+        name="azure_key_credential_policy"
+    ) as default_client:
+        yield default_client
+
+def test_multiapi_credential_default_policy_type(default_client):
+    # making sure that the authentication policy is AzureKeyCredentialPolicy
+    assert isinstance(default_client._config.authentication_policy, AzureKeyCredentialPolicy)
---
+++ test/multiapi/specification/multiapicredentialdefaultpolicy/README.md
@@ -0,0 +1,50 @@
+# Testing multiapi
+
+### Tag: v1
+
+These settings apply only when `--tag=v1` is specified on the command line.
+
+``` yaml $(tag) == 'v1'
+input-file: ../../../../node_modules/@microsoft.azure/autorest.testserver/swagger/multiapi-v1.json
+namespace: multiapicredentialdefaultpolicy.v1
+output-folder: $(python-sdks-folder)/multiapi/Expected/AcceptanceTests/MultiapiCredentialDefaultPolicy/multiapicredentialdefaultpolicy/v1
+```
+
+``` yaml $(tag) == 'v2'
+input-file: ../../../../node_modules/@microsoft.azure/autorest.testserver/swagger/multiapi-v2.json
+namespace: multiapicredentialdefaultpolicy.v2
+output-folder: $(python-sdks-folder)/multiapi/Expected/AcceptanceTests/MultiapiCredentialDefaultPolicy/multiapicredentialdefaultpolicy/v2
+```
+
+``` yaml $(tag) == 'v3'
+input-file: ../../../../node_modules/@microsoft.azure/autorest.testserver/swagger/multiapi-v3.json
+namespace: multiapicredentialdefaultpolicy.v3
+output-folder: $(python-sdks-folder)/multiapi/Expected/AcceptanceTests/MultiapiCredentialDefaultPolicy/multiapicredentialdefaultpolicy/v3
+```
+
+### Settings
+``` yaml
+package-name: multiapicredentialdefaultpolicy
+no-namespace-folders: true
+license-header: MICROSOFT_MIT_NO_VERSION
+azure: true
+add-credentials: true
+credential-default-policy-type: AzureKeyCredentialPolicy
+```
+
+``` yaml $(multiapi)
+clear-output-folder: true
+batch:
+    - tag: v1
+    - tag: v2
+    - tag: v3
+    - multiapiscript: true
+```
+
+### Multi-api script
+
+``` yaml $(multiapiscript)
+output-folder: $(python-sdks-folder)/multiapi/Expected/AcceptanceTests/MultiapiCredentialDefaultPolicy/multiapicredentialdefaultpolicy/
+clear-output-folder: false
+perform-load: false
+```
