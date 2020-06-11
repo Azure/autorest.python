a1db3e65c1960d676a24b94665ad6878e0242938
iscai-msft <iscai@microsoft.com>
2020-06-11 18:32:53 -0400

    correct generation for multiapi lro and paging

--- test/multiapi/Expected/AcceptanceTests/MultiapiWithSubmodule/multiapiwithsubmodule/submodule/aio/_operations_mixin_async.py
+++ test/multiapi/Expected/AcceptanceTests/MultiapiWithSubmodule/multiapiwithsubmodule/submodule/aio/_operations_mixin_async.py
@@ -53,6 +53,40 @@ class MultiapiServiceClientOperationsMixin(object):
         mixin_instance._deserialize = Deserializer(self._models_dict(api_version))
         return await mixin_instance.begin_test_lro(product, **kwargs)
 
+    def begin_test_lro_and_paging(
+        self,
+        client_request_id: Optional[str] = None,
+        test_lro_and_paging_options: Optional["models.TestLroAndPagingOptions"] = None,
+        **kwargs
+    ) -> AsyncLROPoller[AsyncItemPaged["models.PagingResult"]]:
+        """A long-running paging operation that includes a nextLink that has 10 pages.
+
+        :param client_request_id:
+        :type client_request_id: str
+        :param test_lro_and_paging_options: Parameter group.
+        :type test_lro_and_paging_options: ~multiapiwithsubmodule.submodule.v1.models.TestLroAndPagingOptions
+        :keyword callable cls: A custom type or function that will be passed the direct response
+        :keyword str continuation_token: A continuation token to restart a poller from a saved state.
+        :keyword polling: True for ARMPolling, False for no polling, or a
+         polling object for personal polling strategy
+        :paramtype polling: bool or ~azure.core.polling.AsyncPollingMethod
+        :keyword int polling_interval: Default waiting time between two polls for LRO operations if no Retry-After header is present.
+        :return: An instance of AsyncLROPoller that returns an iterator like instance of either PagingResult or the result of cls(response)
+        :rtype: ~azure.core.polling.AsyncLROPoller[~azure.core.async_paging.AsyncItemPaged[~multiapiwithsubmodule.submodule.v1.models.PagingResult]]
+        :raises ~azure.core.exceptions.HttpResponseError:
+        """
+        api_version = self._get_api_version('begin_test_lro_and_paging')
+        if api_version == '1.0.0':
+            from ..v1.aio.operations_async import MultiapiServiceClientOperationsMixin as OperationClass
+        else:
+            raise NotImplementedError("APIVersion {} is not available".format(api_version))
+        mixin_instance = OperationClass()
+        mixin_instance._client = self._client
+        mixin_instance._config = self._config
+        mixin_instance._serialize = Serializer(self._models_dict(api_version))
+        mixin_instance._deserialize = Deserializer(self._models_dict(api_version))
+        return mixin_instance.begin_test_lro_and_paging(client_request_id, test_lro_and_paging_options, **kwargs)
+
     async def test_one(
         self,
         id: int,
