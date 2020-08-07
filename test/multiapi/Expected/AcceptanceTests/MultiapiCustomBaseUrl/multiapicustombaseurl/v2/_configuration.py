2c0a39a61454a70b6fb2ca421840936c97395c10
iscai-msft <iscai@microsoft.com>
2020-08-07 12:40:55 -0400

    regenerate

--- test/multiapi/Expected/AcceptanceTests/MultiapiCustomBaseUrl/multiapicustombaseurl/v2/_configuration.py
+++ test/multiapi/Expected/AcceptanceTests/MultiapiCustomBaseUrl/multiapicustombaseurl/v2/_configuration.py
@@ -47,8 +47,7 @@ class MultiapiCustomBaseUrlServiceClientConfiguration(Configuration):
         self.credential = credential
         self.endpoint = endpoint
         self.api_version = "2.0.0"
-        self.credential_scopes = []
-        self.credential_scopes.extend(kwargs.pop('credential_scopes', []))
+        self.credential_scopes = kwargs.pop('credential_scopes', [])
         kwargs.setdefault('sdk_moniker', 'multiapicustombaseurl/{}'.format(VERSION))
         self._configure(**kwargs)
 
