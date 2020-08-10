9dbe2d17f190820042bda4b2f54d53c563b1df98
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2020-08-10 17:02:35 -0400

    update azure-core and msrest versions (#747)
    * add template code for new azure core and msrest dependencies
    
    * regenerate
    
    * update changelog

--- test/vanilla/Expected/AcceptanceTests/BodyString/bodystring/operations/_enum_operations.py
+++ test/vanilla/Expected/AcceptanceTests/BodyString/bodystring/operations/_enum_operations.py
@@ -97,7 +97,7 @@ class EnumOperations(object):
         # type: (...) -> None
         """Sends value 'red color' from enumeration of 'red color', 'green-color', 'blue_color'.
 
-        :param string_body:
+        :param string_body: string body.
         :type string_body: str or ~bodystring.models.Colors
         :keyword callable cls: A custom type or function that will be passed the direct response
         :return: None, or the result of cls(response)
@@ -190,7 +190,7 @@ class EnumOperations(object):
         # type: (...) -> None
         """Sends value 'red color' from enumeration of 'red color', 'green-color', 'blue_color'.
 
-        :param enum_string_body:
+        :param enum_string_body: enum string body.
         :type enum_string_body: str or ~bodystring.models.Colors
         :keyword callable cls: A custom type or function that will be passed the direct response
         :return: None, or the result of cls(response)
