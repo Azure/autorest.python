811d366a78c8b584cf674a1a3d1e1c79a00d52df
iscai-msft <iscai@microsoft.com>
2020-12-09 11:24:55 -0500

    regenerate with new enum description

--- test/vanilla/Expected/AcceptanceTests/ExtensibleEnums/extensibleenumsswagger/models/_pet_store_inc_enums.py
+++ test/vanilla/Expected/AcceptanceTests/ExtensibleEnums/extensibleenumsswagger/models/_pet_store_inc_enums.py
@@ -40,7 +40,8 @@ class DaysOfWeekExtensibleEnum(with_metaclass(_CaseInsensitiveEnumMeta, str, Enu
 
 class IntEnum(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
-    #: one.
+    #: This is a really long comment to see what wrapping looks like. This comment is really long and
+    #: it should wrap for readability. Please wrap. This should wrap.
     ONE = "1"
     #: two.
     TWO = "2"
