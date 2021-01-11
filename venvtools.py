3e8137c416b55d338c45f6e6d6588ac63552a343
iscai-msft <iscai@microsoft.com>
2021-01-11 15:49:05 -0500

    remove default [] for empty argument

--- venvtools.py
+++ venvtools.py
@@ -56,12 +56,12 @@ def create_venv_with_package(packages):
             subprocess.check_call(pip_call + packages)
         yield myenv
 
-def python_run(venv_context, module, command=[], *, additional_dir=".", error_ok=False):
+def python_run(venv_context, module, command=None, *, additional_dir=".", error_ok=False):
     try:
         cmd_line= [
             venv_context.env_exe,
             "-m", module
-        ] + command
+        ] + (command if command else [])
         print("Executing: {}".format(" ".join(cmd_line)), file=sys.stderr)
         subprocess.run(
             cmd_line,
