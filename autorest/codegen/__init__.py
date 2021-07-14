8235624b0b12016571cf699990ad80f0178a3765
iscai-msft <iscai@microsoft.com>
2021-07-14 14:27:23 -0400

    don't force flags in llc mode

--- autorest/codegen/__init__.py
+++ autorest/codegen/__init__.py
@@ -83,19 +83,26 @@ class CodeGenerator(Plugin):
     def _create_code_model(self, yaml_data: Dict[str, Any], options: Dict[str, Union[str, bool]]) -> CodeModel:
         # Create a code model
         low_level_client = self._autorestapi.get_boolean_value("low-level-client", False)
-        show_models = self._autorestapi.get_boolean_value("show-models", True)
-        show_builders = self._autorestapi.get_boolean_value("show-builders", False)
-        show_operations = self._autorestapi.get_boolean_value("show-operations", True)
-        show_send_request = self._autorestapi.get_boolean_value("show-send-request", False)
-        only_path_and_body_params_positional = self._autorestapi.get_boolean_value(
-            "only-path-and-body-params-positional", False
+        show_models = self._autorestapi.get_boolean_value(
+            "show-models",
+            not low_level_client
         )
-        if low_level_client:
-            show_models = False
-            show_builders = True
-            show_operations = False
-            show_send_request = True
-            only_path_and_body_params_positional = True
+        show_builders = self._autorestapi.get_boolean_value(
+            "show-builders",
+            low_level_client
+        )
+        show_operations = self._autorestapi.get_boolean_value(
+            "show-operations",
+            not low_level_client
+        )
+        show_send_request = self._autorestapi.get_boolean_value(
+            "show-send-request",
+            low_level_client
+        )
+        only_path_and_body_params_positional = self._autorestapi.get_boolean_value(
+            "only-path-and-body-params-positional",
+            low_level_client
+        )
         code_model = CodeModel(
             show_builders=show_builders,
             show_models=show_models,
