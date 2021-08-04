b4fbe784a2305d5a02f76da6a8dbc882109c2556
msyyc <70930885+msyyc@users.noreply.github.com>
2021-08-04 11:21:00 +0800

    inv regenerate

--- test/multiapi/Expected/AcceptanceTests/Multiapi/multiapi/v3/_configuration.py
+++ test/multiapi/Expected/AcceptanceTests/Multiapi/multiapi/v3/_configuration.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING
 
 from azure.core.configuration import Configuration
 from azure.core.pipeline import policies
-from azure.mgmt.core.policies import ARMHttpLoggingPolicy, ARMChallengeAuthenticationPolicy
+from azure.mgmt.core.policies import ARMChallengeAuthenticationPolicy, ARMHttpLoggingPolicy
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
