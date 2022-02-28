7ba41602da6205a05b36c460cb70ef6c4537a5d3
iscai-msft <iscai@microsoft.com>
2022-02-28 12:35:41 -0500

    don't always delete patch file

--- autorest/codegen/serializers/__init__.py
+++ autorest/codegen/serializers/__init__.py
@@ -93,12 +93,10 @@ class JinjaSerializer:
 
 
     def _keep_patch_file(self, path_file: Path, env: Environment):
-        # if self._autorestapi.read_file(path_file):
-        #     self._autorestapi.write_file(path_file, self._autorestapi.read_file(path_file))
-        # else:
-        #     self._autorestapi.write_file(path_file, PatchSerializer(env=env).serialize())
-        self._autorestapi.write_file(path_file, PatchSerializer(env=env).serialize())
-
+        if self._autorestapi.read_file(path_file):
+            self._autorestapi.write_file(path_file, self._autorestapi.read_file(path_file))
+        else:
+            self._autorestapi.write_file(path_file, PatchSerializer(env=env).serialize())
 
     def _serialize_and_write_models_folder(self, env: Environment, namespace_path: Path) -> None:
         # Write the models folder
