f1ffcd37ed0398adeb224e34d52cd7054c3afcf5
msyyc <70930885+msyyc@users.noreply.github.com>
2022-03-09 15:24:48 +0800

    aio test

---
+++ test/vanilla/legacy/AcceptanceTests/asynctests/test_security.py
@@ -0,0 +1,48 @@
+# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# --------------------------------------------------------------------------
+import pytest
+from securityaad.aio import SecurityAadClient
+from securityazurekey.aio import SecurityAzureKeyClient
+from securitymixed.aio import SecurityMixedClient
+from azure.core.credentials import AzureKeyCredential
+from azure.core.pipeline.policies import AzureKeyCredentialPolicy
+from azure.core.pipeline.policies import AsyncBearerTokenCredentialPolicy
+from azure.identity import DefaultAzureCredential
+
+@pytest.mark.asyncio
+async def test_security_azure_key():
+    client = SecurityAzureKeyClient(credential=AzureKeyCredential('000'))
+    assert isinstance(client._config.authentication_policy, AzureKeyCredentialPolicy)
+
+@pytest.mark.asyncio
+async def test_security_aad():
+    client = SecurityAadClient(credential=DefaultAzureCredential())
+    assert isinstance(client._config.authentication_policy, AsyncBearerTokenCredentialPolicy)
+
+@pytest.mark.asyncio
+async def test_security_mixed():
+    client = SecurityMixedClient(credential=DefaultAzureCredential())
+    assert isinstance(client._config.authentication_policy, AsyncBearerTokenCredentialPolicy)
