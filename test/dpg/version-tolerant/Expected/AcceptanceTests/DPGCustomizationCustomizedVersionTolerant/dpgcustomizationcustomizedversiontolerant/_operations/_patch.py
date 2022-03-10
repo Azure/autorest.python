d6babe3f1e9bc442af25792a6d5c62f032f46554
iscai-msft <iscai@microsoft.com>
2022-03-09 19:09:50 -0500

    update patch_sdk documentation

--- test/dpg/version-tolerant/Expected/AcceptanceTests/DPGCustomizationCustomizedVersionTolerant/dpgcustomizationcustomizedversiontolerant/_operations/_patch.py
+++ test/dpg/version-tolerant/Expected/AcceptanceTests/DPGCustomizationCustomizedVersionTolerant/dpgcustomizationcustomizedversiontolerant/_operations/_patch.py
@@ -86,7 +86,12 @@ class DPGClientOperationsMixin(DPGClientOperationsMixinGenerated):
 
 
 def patch_sdk():
-    """Do not remove from this file"""
+    """Do not remove from this file.
+
+    `patch_sdk` is a last resort escape hatch that allows you to do customizations
+    you can't otherwise do with the following handwritten customizations framework
+    aka.ms/azsdk/python/dpcodegen/python/customize
+    """
     pass
 
 
--- test/multiapi/Expected/AcceptanceTests/Multiapi/multiapi/v3/aio/operations/_patch.py
+++ test/multiapi/Expected/AcceptanceTests/Multiapi/multiapi/v3/aio/operations/_patch.py
@@ -7,7 +7,12 @@
 # Follow our quickstart here for examples: aka.ms/azsdk/python/dpcodegen/python/customize
 
 def patch_sdk():
-    """Do not remove from this file"""
+    """Do not remove from this file.
+
+    `patch_sdk` is a last resort escape hatch that allows you to do customizations
+    you can't otherwise do with the following handwritten customizations framework
+    aka.ms/azsdk/python/dpcodegen/python/customize
+    """
     pass
 
 __all__ = []  # only add objects you want to be publicly available to your users at your package level
