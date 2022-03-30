6e9b2dac902473ecf46c15cc22da8647ffb02698
Changlong Liu <changlong.liu@microsoft.com>
2022-03-30 14:37:35 +0800

    change-customization

--- test/dpg/version-tolerant/Expected/AcceptanceTests/DPGCustomizationCustomizedVersionTolerant/dpgcustomizationcustomizedversiontolerant/aio/_operations/_patch.py
+++ test/dpg/version-tolerant/Expected/AcceptanceTests/DPGCustomizationCustomizedVersionTolerant/dpgcustomizationcustomizedversiontolerant/aio/_operations/_patch.py
@@ -3,14 +3,11 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-from typing import Any, AsyncIterable, TYPE_CHECKING
+from typing import Any, AsyncIterable, TYPE_CHECKING, Union, cast
 from azure.core.polling import AsyncLROPoller
 
-from ._operations import DPGClientOperationsMixin as DPGClientOperationsMixinGenerated
+from ._operations import DPGClientOperationsMixin as DPGClientOperationsMixinGenerated, JSONObject
 from ...models import *  # pylint: disable=wildcard-import,unused-wildcard-import
-
-if TYPE_CHECKING:
-    from typing import Literal
 
 
 class DPGClientOperationsMixin(DPGClientOperationsMixinGenerated):
@@ -18,21 +15,23 @@ class DPGClientOperationsMixin(DPGClientOperationsMixinGenerated):
         response = await super().get_model(mode, **kwargs)
         return Product(**response)
 
-    async def post_model(self, mode: str, input: Product, **kwargs: Any) -> Product:
+    async def post_model(self, mode: str, input: Union[Product, JSONObject], **kwargs: Any) -> Product:
         response = await super().post_model(mode, input, **kwargs)
         return Product(**response)
 
     def get_pages(self, mode: str, **kwargs) -> AsyncIterable[Product]:
-        return super().get_pages(mode, cls=lambda objs: [Product(**x) for x in objs], **kwargs)
+        pages = super().get_pages(mode, cls=lambda objs: [Product(**x) for x in objs], **kwargs)
+        return cast(AsyncIterable[Product], pages) # be safe since calling get_pages with Product
 
-    async def begin_lro(self, mode: str, **kwargs: Any) -> AsyncLROPoller[LROProduct]:
-        return await super().begin_lro(
+    async def begin_lro(self, mode: str, **kwargs: Any) -> AsyncLROPoller[LROProduct]: # type: ignore
+        lro = await super().begin_lro(
             mode,
             cls=lambda pipeline_response, deserialized, headers: LROProduct._from_dict(  # pylint: disable=protected-access
                 **deserialized
             ),
             **kwargs
         )
+        return cast(AsyncLROPoller[LROProduct], lro) # be safe since calling begin_lro with LROProduct
 
 
 def patch_sdk():
