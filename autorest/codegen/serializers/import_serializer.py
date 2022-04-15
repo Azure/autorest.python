ef10f7ad242fb8ae7fc5757e66eeaa4e124db4a5
iscai-msft <iscai@microsoft.com>
2022-04-15 15:22:42 -0700

    black code

--- autorest/codegen/serializers/import_serializer.py
+++ autorest/codegen/serializers/import_serializer.py
@@ -130,7 +130,9 @@ class FileImportSerializer:
                             )
                         )
                         if i > 0:
-                            ret[-1] += "  # type: ignore  # pylint: disable=ungrouped-imports"
+                            ret[
+                                -1
+                            ] += "  # type: ignore  # pylint: disable=ungrouped-imports"
             ret.append("{}{} = {}".format(spacing, type_name, definition_value))
             return ret
 
