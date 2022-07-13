e3611fa5610b2211a79a0efd654ef7f062136093
iscai-msft <iscai@microsoft.com>
2022-07-13 13:12:41 -0700

    drop 3.6 support

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
