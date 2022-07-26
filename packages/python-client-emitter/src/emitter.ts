c4a3146a89c65635ce56ef1c630e11fe4e7bae76
iscai-msft <iscai@microsoft.com>
2022-07-25 17:09:42 -0700

    add emitter

--- packages/python-client-emitter/src/emitter.ts
+++ packages/python-client-emitter/src/emitter.ts
@@ -30,16 +30,20 @@ import { dump } from "js-yaml";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import getAutorestPath from "../../get-autorest-python-path.cjs";
+
 export async function $onEmit(program: Program) {
   const yamlMap = createYamlEmitter(program);
   const yamlPath = resolvePath(program.compilerOptions.outputPath!, "output.yaml");
   await program.host.writeFile(yamlPath, dump(yamlMap));
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const root = resolve(__dirname, "..", "..");
+  const autorestPath = getAutorestPath();
+  console.log(autorestPath);
 
   execFileSync(process.execPath, [
-    `${root}/node_modules/@autorest/python/run-python3.js`,
-    `${root}/node_modules/@autorest/python/run_cadl.py`,
+    `${autorestPath}/run-python3.js`,
+    `${autorestPath}/run_cadl.py`,
     `--output-folder=${program.compilerOptions.outputPath!}`,
     `--cadl-file=${yamlPath}`,
   ]);
