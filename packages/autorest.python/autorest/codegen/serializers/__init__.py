7d36750cf213ef95cfbe2fd547323637a9d68750
Changlong Liu <changlong.liu@microsoft.com>
2022-08-26 19:07:46 +0800

    Revert "generate only has model"
    This reverts commit 6c5b454b5f93f33cc63da59f059d86a061bc1a9e.

--- packages/autorest.python/autorest/codegen/serializers/__init__.py
+++ packages/autorest.python/autorest/codegen/serializers/__init__.py
@@ -427,10 +427,7 @@ class JinjaSerializer(ReaderAndWriter):  # pylint: disable=abstract-method
                 namespace_path / Path("_serialization.py"),
                 general_serializer.serialize_serialization_file(),
             )
-        if (
-            self.code_model.options["models_mode"] == "dpg"
-            and self.code_model.model_types
-        ):
+        if self.code_model.options["models_mode"] == "dpg":
             self.write_file(
                 namespace_path / Path("_model_base.py"),
                 general_serializer.serialize_model_base_file(),
