2675aa67d7f305ce32d16337cf769cbd32c8342c
msyyc <70930885+msyyc@users.noreply.github.com>
2022-08-30 16:11:43 +0800

    anonymous model

--- packages/cadl-python/src/emitter.ts
+++ packages/cadl-python/src/emitter.ts
@@ -347,7 +347,7 @@ function emitResponse(
     let type = undefined;
     if (innerResponse.body?.type) {
         // temporary logic. It can be removed after compiler optimize the response
-        const candidate = ["ResourceOkResponse", "ResourceCreatedResponse"];
+        const candidate = ["ResourceOkResponse", "ResourceCreatedResponse", "AcceptedResponse"];
         const originType = innerResponse.body.type as ModelType;
         if (innerResponse.body.type.kind == "Model" && candidate.find(e => e === originType.name)) {
             const modelType = getEffectiveSchemaType(program, originType);
