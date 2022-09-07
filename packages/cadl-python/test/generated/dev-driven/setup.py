51bf4ac44eefcd2a9ebe46945a163262ed6cf66e
iscai-msft <iscai@microsoft.com>
2022-09-06 17:35:03 -0700

    temp

--- packages/cadl-python/test/generated/dev-driven/setup.py
+++ packages/cadl-python/test/generated/dev-driven/setup.py
@@ -9,12 +9,12 @@
 from setuptools import setup, find_packages
 
 
-PACKAGE_NAME = "resiliencydevdriven"
+PACKAGE_NAME = "microsoftcognitivelanguageserviceanalyzetextauthoring"
 version = "0.1.0"
 setup(
     name=PACKAGE_NAME,
     version=version,
-    description="ResiliencyDevDriven",
+    description="MicrosoftCognitiveLanguageServiceAnalyzeTextAuthoring",
     author_email="",
     url="",
     keywords="azure, azure sdk",
