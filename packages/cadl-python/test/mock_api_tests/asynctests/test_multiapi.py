4ed81f6b92b49a1e0bdcf16941c572733a3293b1
iscai-msft <iscai@microsoft.com>
2022-09-12 11:05:35 -0700

    comment out calls for now

--- packages/cadl-python/test/mock_api_tests/asynctests/test_multiapi.py
+++ packages/cadl-python/test/mock_api_tests/asynctests/test_multiapi.py
@@ -4,27 +4,28 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
-from resiliency.servicedriven2 import ResiliencyServiceDriven2
+from resiliency.servicedriven2.aio import ResiliencyServiceDriven2
 
 @pytest.fixture
-def initial_client():
-    with ResiliencyServiceDriven2(api_version="1.0.0") as client:
+async def initial_client():
+    async with ResiliencyServiceDriven2(api_version="1.0.0") as client:
         yield client
 
 @pytest.fixture
-def updated_client():
-    with ResiliencyServiceDriven2() as client:
+async def updated_client():
+    async with ResiliencyServiceDriven2() as client:
         yield client
 
-def test_added_params(initial_client, updated_client):
+@pytest.mark.asyncio
+async def test_added_params(initial_client, updated_client):
     with pytest.raises(ValueError):
-        initial_client.params.get_required(parameter="foo", new_parameter="bar")
+        await initial_client.params.get_required(parameter="foo", new_parameter="bar")
     # initial_client.params.get_required(parameter="foo")
     # updated_client.params.get_required(parameter="foo")
     # updated_client.params.get_required(parameter="foo", new_parameter="bar")
 
-
-def test_added_method(initial_client, updated_client):
+@pytest.mark.asyncio
+async def test_added_method(initial_client, updated_client):
     with pytest.raises(ValueError):
-        initial_client.params.get_new_operation()
+        await initial_client.params.get_new_operation()
     # updated_client.params.get_new_operation()
