52d78c9315adcb106fee4f603181302724de676a
msyyc <70930885+msyyc@users.noreply.github.com>
2022-09-15 12:55:19 +0800

    test

---
+++ packages/cadl-python/test/mock_api_tests/test_lro_basic.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import pytest
+from lro.basic import LroBasic
+
+@pytest.fixture
+def client():
+    with LroBasic() as client:
+        yield client
+
+def test_lro_basic_put(client):
+    result = client.begin_create().result()
+    assert result == "Successful"
