5253defefd02b8d354291c03ebe7d092ff594275
iscai-msft <iscai@microsoft.com>
2022-09-20 13:33:05 -0700

    update property types tests

--- packages/cadl-python/test/mock_api_tests/asynctests/test_property_types_async.py
+++ packages/cadl-python/test/mock_api_tests/asynctests/test_property_types_async.py
@@ -7,13 +7,13 @@ from typing import Any
 import pytest
 from models.property.types import models
 from models.property.types.aio import ModelsPropertyTypes
+import datetime
 
 @pytest.fixture
 async def client():
     async with ModelsPropertyTypes() as client:
         yield client
 
-@pytest.mark.asyncio
 @pytest.mark.parametrize(
 "og_name,val", [
     ("boolean", True),
@@ -21,7 +21,7 @@ async def client():
     ("bytes", "aGVsbG8sIHdvcmxkIQ=="),
     ("int", 42),
     ("float", 42.42),
-    ("datetime", "2022-08-26T18:38:00Z"),
+    ("datetime", "2022-08-26T18:38:00.000Z"),
     ("duration", "P123DT22H14M12.011S"),
     ("enum", "ValueOne"),
     ("extensible_enum", "UnknownValue"),
@@ -31,7 +31,8 @@ async def client():
     ("collections_model", [{'property': 'hello'}, {'property': 'world'}]),
 ]
 )
-async def test(client, og_name, val):
+@pytest.mark.asyncio
+async def test_json(client, og_name, val):
     body = {"property": val}
     og_group = getattr(client, og_name)
     assert await og_group.get() == body
@@ -41,14 +42,12 @@ async def test(client, og_name, val):
 "og_name,model,val", [
     ("boolean", models.BooleanProperty, True),
     ("string", models.StringProperty, "hello"),
-    ("bytes", models.BytesProperty, "aGVsbG8sIHdvcmxkIQ=="),
+    ("bytes", models.BytesProperty, b'hello, world!'),
     ("int", models.IntProperty, 42),
     ("float", models.FloatProperty, 42.42),
-    ("datetime", models.DatetimeProperty, "2022-08-26T18:38:00Z"),
-    ("duration", models.DurationProperty, "P123DT22H14M12.011S"),
-    ("enum", models.EnumProperty, "ValueOne"),
+    ("enum", models.EnumProperty, models.InnerEnum.VALUE_ONE),
     ("extensible_enum", models.ExtensibleEnumProperty, "UnknownValue"),
-    ("model", models.ModelProperty, {'property': 'hello'}),
+    ("model", models.ModelProperty, models.InnerModel(property="hello")),
     ("collections_string", models.CollectionsStringProperty, ['hello', 'world']),
     ("collections_int", models.CollectionsIntProperty, [1, 2]),
     ("collections_model", models.CollectionsModelProperty, [{'property': 'hello'}, {'property': 'world'}]),
@@ -59,4 +58,24 @@ async def test_model(client, og_name, model, val):
     body = model(property=val)
     og_group = getattr(client, og_name)
     assert await og_group.get() == body
+    assert (await og_group.get()).property == val
     await og_group.put(body)
+
+@pytest.mark.asyncio
+async def test_datetime_model(client):
+    received_body = await client.datetime.get()
+    assert received_body == {"property": "2022-08-26T18:38:00.000Z"}
+    assert received_body.property.year == 2022
+    assert received_body.property.month == 8
+    assert received_body.property.day == 26
+    assert received_body.property.hour == 18
+    assert received_body.property.minute == 38
+    await client.datetime.put(models.DatetimeProperty(property=datetime.datetime(2022, 8, 26, hour=18, minute=38)))
+
+# def test_duration_model(client):
+#     received_body = client.duration.get()
+#     assert received_body == {"property": "P123DT22H14M12.011S"}
+#     assert received_body.property.days == 123
+#     assert received_body.property.seconds == 80052
+#     assert received_body.property.microseconds == 11000
+#     client.duration.put(models.DurationProperty(property=datetime.timedelta(days=123, seconds=80052, microseconds=11000)))
--- packages/cadl-python/test/mock_api_tests/test_property_types.py
+++ packages/cadl-python/test/mock_api_tests/test_property_types.py
@@ -6,6 +6,7 @@
 from typing import Any
 import pytest
 from models.property.types import ModelsPropertyTypes, models
+import datetime
 
 @pytest.fixture
 def client():
@@ -19,7 +20,7 @@ def client():
     ("bytes", "aGVsbG8sIHdvcmxkIQ=="),
     ("int", 42),
     ("float", 42.42),
-    ("datetime", "2022-08-26T18:38:00Z"),
+    ("datetime", "2022-08-26T18:38:00.000Z"),
     ("duration", "P123DT22H14M12.011S"),
     ("enum", "ValueOne"),
     ("extensible_enum", "UnknownValue"),
@@ -39,14 +40,12 @@ def test_json(client, og_name, val):
 "og_name,model,val", [
     ("boolean", models.BooleanProperty, True),
     ("string", models.StringProperty, "hello"),
-    ("bytes", models.BytesProperty, "aGVsbG8sIHdvcmxkIQ=="),
+    ("bytes", models.BytesProperty, b'hello, world!'),
     ("int", models.IntProperty, 42),
     ("float", models.FloatProperty, 42.42),
-    ("datetime", models.DatetimeProperty, "2022-08-26T18:38:00Z"),
-    ("duration", models.DurationProperty, "P123DT22H14M12.011S"),
-    ("enum", models.EnumProperty, "ValueOne"),
+    ("enum", models.EnumProperty, models.InnerEnum.VALUE_ONE),
     ("extensible_enum", models.ExtensibleEnumProperty, "UnknownValue"),
-    ("model", models.ModelProperty, {'property': 'hello'}),
+    ("model", models.ModelProperty, models.InnerModel(property="hello")),
     ("collections_string", models.CollectionsStringProperty, ['hello', 'world']),
     ("collections_int", models.CollectionsIntProperty, [1, 2]),
     ("collections_model", models.CollectionsModelProperty, [{'property': 'hello'}, {'property': 'world'}]),
@@ -56,4 +55,23 @@ def test_model(client, og_name, model, val):
     body = model(property=val)
     og_group = getattr(client, og_name)
     assert og_group.get() == body
+    assert og_group.get().property == val
     og_group.put(body)
+
+def test_datetime_model(client):
+    received_body = client.datetime.get()
+    assert received_body == {"property": "2022-08-26T18:38:00.000Z"}
+    assert received_body.property.year == 2022
+    assert received_body.property.month == 8
+    assert received_body.property.day == 26
+    assert received_body.property.hour == 18
+    assert received_body.property.minute == 38
+    client.datetime.put(models.DatetimeProperty(property=datetime.datetime(2022, 8, 26, hour=18, minute=38)))
+
+# def test_duration_model(client):
+#     received_body = client.duration.get()
+#     assert received_body == {"property": "P123DT22H14M12.011S"}
+#     assert received_body.property.days == 123
+#     assert received_body.property.seconds == 80052
+#     assert received_body.property.microseconds == 11000
+#     client.duration.put(models.DurationProperty(property=datetime.timedelta(days=123, seconds=80052, microseconds=11000)))
