79966e212842c886c148ada156acd650f7acd219
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2022-09-23 15:40:27 -0700

    allow users to pass flags through cadl-project (#1467)

--- packages/autorest.python/autorest/m2r/__init__.py
+++ packages/autorest.python/autorest/m2r/__init__.py
@@ -68,5 +68,7 @@ class M2RAutorest(YamlUpdatePluginAutorest, M2R):
 
 if __name__ == "__main__":
     # CADL pipeline will call this
-    args = parse_args()
-    M2R(output_folder=args.output_folder, cadl_file=args.cadl_file).process()
+    args, unknown_args = parse_args()
+    M2R(
+        output_folder=args.output_folder, cadl_file=args.cadl_file, **unknown_args
+    ).process()
