d8746fab5401566d40e902950a9776c3fbc50d45
Yuchao Yan <yuchaoyan@microsoft.com>
2022-10-13 14:20:21 +0800

    Update utils.py

--- packages/autorest.python/autorest/codegen/serializers/utils.py
+++ packages/autorest.python/autorest/codegen/serializers/utils.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import re
 
 
 def method_signature_and_response_type_annotation_template(
