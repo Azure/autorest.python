7a2b1c521b458f5e20d72a96bc5ca54d2823b940
iscai-msft <iscai@microsoft.com>
2022-10-17 14:15:10 -0700

    regen samples

--- packages/autorest.python/samples/specification/multiapi/generated/azure/multiapi/sample/v1/operations/_multiapi_service_client_operations.py
+++ packages/autorest.python/samples/specification/multiapi/generated/azure/multiapi/sample/v1/operations/_multiapi_service_client_operations.py
@@ -29,7 +29,7 @@ from azure.mgmt.core.polling.arm_polling import ARMPolling
 
 from .. import models as _models
 from ..._serialization import Serializer
-from .._vendor import MixinABC, _convert_request
+from .._vendor import MultiapiServiceClientMixinABC, _convert_request
 
 T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
@@ -119,7 +119,7 @@ def build_test_different_calls_request(*, greeting_in_english: str, **kwargs: An
     return HttpRequest(method="GET", url=_url, params=_params, headers=_headers, **kwargs)
 
 
-class MultiapiServiceClientOperationsMixin(MixinABC):
+class MultiapiServiceClientOperationsMixin(MultiapiServiceClientMixinABC):
     @distributed_trace
     def test_one(  # pylint: disable=inconsistent-return-statements
         self, id: int, message: Optional[str] = None, **kwargs: Any
