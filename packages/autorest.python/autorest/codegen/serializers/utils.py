abde23ed99a8a01af854611768f1c2f48dc33b7b
Yuchao Yan <yuchaoyan@microsoft.com>
2022-11-10 05:51:45 +0800

    optimize rule of output file name for `--generate-sample` (#1540)

--- packages/autorest.python/autorest/codegen/serializers/utils.py
+++ packages/autorest.python/autorest/codegen/serializers/utils.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+from pathlib import Path
 
 
 def method_signature_and_response_type_annotation_template(
@@ -11,3 +12,8 @@ def method_signature_and_response_type_annotation_template(
     response_type_annotation: str,
 ) -> str:
     return f"{method_signature} -> {response_type_annotation}:"
+
+
+def extract_sample_name(file_path: str) -> str:
+    file = file_path.split("specification")[-1]
+    return Path(file).parts[-1].replace(".json", "")
