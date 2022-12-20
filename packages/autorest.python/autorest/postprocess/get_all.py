ba5d2b88a738948eac91288859eb48a341927dfd
Laurent Mazuel <laurent.mazuel@gmail.com>
2022-12-20 13:10:50 -0800

    Black

--- packages/autorest.python/autorest/postprocess/get_all.py
+++ packages/autorest.python/autorest/postprocess/get_all.py
@@ -15,5 +15,7 @@ def main(namespace):
 if __name__ == "__main__":
     patched = ",".join(main(sys.argv[1]))
     output_folder = sys.argv[2]
-    with open(f"{output_folder}/.temp_folder/patched.txt", "w", encoding="utf-8-sig") as f:
+    with open(
+        f"{output_folder}/.temp_folder/patched.txt", "w", encoding="utf-8-sig"
+    ) as f:
         f.write(patched)
