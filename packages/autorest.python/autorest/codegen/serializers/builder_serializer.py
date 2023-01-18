7bf45dcde6a1d3aef95b06a14ee8f1b1edd2db25
Changlong Liu <changlong.liu@microsoft.com>
2023-01-18 19:47:55 +0800

    mypy

--- packages/autorest.python/autorest/codegen/serializers/builder_serializer.py
+++ packages/autorest.python/autorest/codegen/serializers/builder_serializer.py
@@ -161,7 +161,7 @@ def _serialize_json_model_body(body_parameter: BodyParameter) -> List[str]:
         for property_name, parameter_name in body_parameter.property_to_parameter_name.items()
     )
     model_type = cast(ModelType, body_parameter.type)
-    if isinstance(model_type, CombinedType):
+    if isinstance(model_type, CombinedType) and model_type.json_subtype:
         model_type = model_type.json_subtype
     retval.append(f"    {body_parameter.client_name} = {{{parameter_string}}}")
     retval.append(f"    {body_parameter.client_name} =  {{")
@@ -365,9 +365,9 @@ class _BuilderBaseSerializer(Generic[BuilderType]):  # pylint: disable=abstract-
 
         json_type = body_param.type
         if isinstance(body_param.type, CombinedType):
-            json_type = body_param.type.json_subtype
-            if json_type is None:
+            if body_param.type.json_subtype is None:
                 return template
+            json_type = body_param.type.json_subtype
 
         polymorphic_subtypes: List[ModelType] = []
         json_type.get_polymorphic_subtypes(polymorphic_subtypes)
