094b9e9b7af8d3153bb348aff7e3c55d07151907
tadelesh <tadelesh.shi@live.cn>
2023-04-12 20:32:52 +0800

    upgrade typespec and adjust test structure

---
+++ packages/typespec-python/test/mock_api_tests/test_model_property_nullable.py
@@ -0,0 +1,47 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from typing import Any
+import pytest
+from models.property.nullable import NullableClient, models
+# from azure.core.serialization import NULL
+
+
+@pytest.fixture
+def client():
+    with NullableClient() as client:
+        yield client
+
+
+@pytest.mark.parametrize(
+    "og_name,model,val",
+    [
+        ("string", models.StringProperty, "hello"),
+        ("bytes", models.BytesProperty, "aGVsbG8sIHdvcmxkIQ=="),
+        ("datetime", models.DatetimeProperty, "2022-08-26T18:38:00Z"),
+        ("duration", models.DurationProperty, "P123DT22H14M12.011S"),
+        (
+            "collections_byte",
+            models.CollectionsByteProperty,
+            ["aGVsbG8sIHdvcmxkIQ==", "aGVsbG8sIHdvcmxkIQ=="],
+        ),
+        # (
+        #     "collections_model",
+        #     models.CollectionsModelProperty,
+        #     [
+        #         models.InnerModel(property="hello"),
+        #         models.InnerModel(property="world"),
+        #     ],
+        # ),
+    ],
+)
+def test(client, og_name, model, val):
+    og_group = getattr(client, og_name)
+    non_null_model = model(required_property="foo", nullable_property=val)
+    # non_model = model(required_property="foo", nullable_property=NULL)
+    assert og_group.get_non_null() == non_null_model
+    assert og_group.get_null()["nullableProperty"] is None
+    og_group.patch_non_null(body=non_null_model)
+    # og_group.patch_null(body=non_model)
