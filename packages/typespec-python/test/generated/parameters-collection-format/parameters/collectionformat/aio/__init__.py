2a6791dd9dff3975389a0852fdbd5e00382ca480
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2023-04-12 17:49:27 -0700

    add collection format tests (#1853)

--- packages/typespec-python/test/generated/parameters-collection-format/parameters/collectionformat/aio/__init__.py
+++ packages/typespec-python/test/generated/parameters-collection-format/parameters/collectionformat/aio/__init__.py
@@ -6,25 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._models import BytesProperty
-from ._models import CollectionsByteProperty
-from ._models import CollectionsModelProperty
-from ._models import DatetimeProperty
-from ._models import DurationProperty
-from ._models import InnerModel
-from ._models import StringProperty
-from ._patch import __all__ as _patch_all
-from ._patch import *  # pylint: disable=unused-wildcard-import
+from ._client import CollectionFormatClient
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "BytesProperty",
-    "CollectionsByteProperty",
-    "CollectionsModelProperty",
-    "DatetimeProperty",
-    "DurationProperty",
-    "InnerModel",
-    "StringProperty",
+    "CollectionFormatClient",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
+
 _patch_sdk()
