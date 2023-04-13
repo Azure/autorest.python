c8702c94c5dbee82c26ef0123a6f9c95d7631d26
tadelesh <tadelesh.shi@live.cn>
2023-04-13 14:42:09 +0800

    fix dpg null serialization bug and add test

--- packages/typespec-python/test/mock_api_tests/test_model_property_nullable.py
+++ packages/typespec-python/test/mock_api_tests/test_model_property_nullable.py
@@ -3,10 +3,14 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import json
 from typing import Any
 import pytest
 from models.property.nullable import NullableClient, models
-# from azure.core.serialization import NULL
+from azure.core.serialization import NULL
+from models.property.nullable._model_base import (  # pylint: disable=protected-access
+    AzureJSONEncoder,
+)
 
 
 @pytest.fixture
@@ -27,21 +31,24 @@ def client():
             models.CollectionsByteProperty,
             ["aGVsbG8sIHdvcmxkIQ==", "aGVsbG8sIHdvcmxkIQ=="],
         ),
-        # (
-        #     "collections_model",
-        #     models.CollectionsModelProperty,
-        #     [
-        #         models.InnerModel(property="hello"),
-        #         models.InnerModel(property="world"),
-        #     ],
-        # ),
+        (
+            "collections_model",
+            models.CollectionsModelProperty,
+            [
+                models.InnerModel(property="hello"),
+                models.InnerModel(property="world"),
+            ],
+        ),
     ],
 )
 def test(client, og_name, model, val):
     og_group = getattr(client, og_name)
     non_null_model = model(required_property="foo", nullable_property=val)
-    # non_model = model(required_property="foo", nullable_property=NULL)
+    non_model = model(required_property="foo", nullable_property=NULL)
+    assert '{"requiredProperty": "foo", "nullableProperty": null}' == json.dumps(
+        non_model, cls=AzureJSONEncoder
+    )
     assert og_group.get_non_null() == non_null_model
     assert og_group.get_null()["nullableProperty"] is None
     og_group.patch_non_null(body=non_null_model)
-    # og_group.patch_null(body=non_model)
+    og_group.patch_null(body=non_model)
