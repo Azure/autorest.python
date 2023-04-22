5b10cc9a4a3f670701209c990af76dec9d5100ec
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2023-04-21 21:26:40 -0700

    update tests to latest  (#1871)

---
+++ packages/typespec-python/test/mock_api_tests/asynctests/test_type_model_usage_async.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import pytest
+from type.model.usage import models
+from type.model.usage.aio import UsageClient
+
+@pytest.fixture
+async def client():
+    async with UsageClient() as client:
+        yield client
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+"op_name, input,output", [
+    ("input", models.InputRecord(required_prop="example-value"), None),
+    ("output", None, models.OutputRecord(required_prop="example-value")),
+    ("input_and_output", models.InputOutputRecord(required_prop="example-value"), models.InputOutputRecord(required_prop="example-value")),
+]
+)
+async def test_input_output(client, op_name, input, output):
+    op = getattr(client, op_name)
+    if input:
+        assert output == await op(input)
+    else:
+        assert output == await op()
