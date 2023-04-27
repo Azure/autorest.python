95167886ef98c8ddf1487ea92a61e189348dd2a7
Yuchao Yan <yuchaoyan@microsoft.com>
2023-04-28 00:35:11 +0800

    Add testcase for @internal (#1877)

--- packages/typespec-python/tasks.py
+++ packages/typespec-python/tasks.py
@@ -99,7 +99,6 @@ def regenerate(c, name=None, debug=False):
         for s in CADL_RANCH_DIR.glob("**/*")
         if s.is_dir()
         and any(f for f in s.iterdir() if f.name == "main.tsp")
-        and "internal" not in s.name.lower()
     ]
     if name:
         specs = [s for s in specs if name.lower() in str(s)]
