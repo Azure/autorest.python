0eefe82c4fb9667dd84bddc3e761e35bb1a37f64
Timothee Guerin <tiguerin@microsoft.com>
2023-05-02 13:54:43 -0700

    Wrong config

--- packages/typespec-python/.eslintrc.cjs
+++ packages/typespec-python/.eslintrc.cjs
@@ -3,7 +3,7 @@ require("@typespec/eslint-config-typespec/patch/modern-module-resolution");
 module.exports = {
     extends: "@typespec/eslint-config-typespec",
     parserOptions: { tsconfigRootDir: __dirname },
-    import: ["deprecation"],
+    plugins: ["deprecation"],
     rules: {
         "deprecation/deprecation": "warn"
     },
