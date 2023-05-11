611efef59886ed9b1d2aef8765d33dc822f08082
msyyc <70930885+msyyc@users.noreply.github.com>
2023-05-11 15:10:01 +0800

    only format .py files

--- packages/autorest.python/autorest/black/__init__.py
+++ packages/autorest.python/autorest/black/__init__.py
@@ -34,24 +34,17 @@ class BlackScriptPlugin(Plugin):  # pylint: disable=abstract-method
         list(
             map(
                 self.format_file,
-                [Path(f) for f in self.list_file() if "__pycache__" not in f],
+                [
+                    Path(f)
+                    for f in self.list_file()
+                    if "__pycache__" not in f and Path(f).suffix == ".py"
+                ],
             )
         )
         return True
 
     def format_file(self, file: Path) -> None:
-        try:
-            file_content = self.read_file(file)
-        except Exception as e:  # pylint: disable=broad-except
-            if file.suffix != ".py":
-                _LOGGER.warning(
-                    "Can not read file %s, not blacking this file", file.name
-                )
-                return
-            raise e  # still want to raise if we fail reading a py file
-        if file.suffix != ".py":
-            self.write_file(file, file_content)
-            return
+        file_content = self.read_file(file)
         try:
             file_content = black.format_file_contents(
                 file_content, fast=True, mode=_BLACK_MODE
