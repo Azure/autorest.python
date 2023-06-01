54673c8db19308428703ac57be52c230df87872f
Yuchao Yan <yuchaoyan@microsoft.com>
2023-06-01 14:07:04 +0800

    Update tasks.py

--- packages/typespec-python/tasks.py
+++ packages/typespec-python/tasks.py
@@ -98,7 +98,7 @@ def regenerate(c, name=None, debug=False):
         s / "main.tsp"
         for s in CADL_RANCH_DIR.glob("**/*")
         if s.is_dir()
-        and any(f for f in s.iterdir() if f.name == "main.tsp" and "authentication/http/custom" not in s.as_posix() )
+        and any(f for f in s.iterdir() if f.name == "main.tsp" and "authentication/http/custom" not in s.as_posix())
     ]
     if name:
         specs = [s for s in specs if name.lower() in str(s)]
