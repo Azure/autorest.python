d902099d524ae432f13ca1256979953c681c8e94
msyyc <70930885+msyyc@users.noreply.github.com>
2023-06-12 12:49:39 +0800

    update

--- packages/autorest.python/test/multiapi/run_multiapi_combiner.py
+++ packages/autorest.python/test/multiapi/run_multiapi_combiner.py
@@ -20,7 +20,6 @@ def main():
         call("git clone --filter=blob:none --sparse https://github.com/Azure/azure-sdk-for-python.git", shell=True)
         os.chdir("azure-sdk-for-python")
         check_call("git sparse-checkout add tools/azure-sdk-tools", shell=True)
-        check_call("git pull origin main", shell=True)
 
         # install multiapi_combiner in venv
         env_builder = venv.EnvBuilder(with_pip=True)
