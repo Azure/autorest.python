Azure/autorest.python

commit 98f6a904a33bc9892ccb12e801f3f7b53f07b373

[typespec] support global config for `head-as-boolean` (#1957)
* open http auth test

* update

* updat test

* udpate test

* Update CHANGELOG.md

packages/typespec-python/tasks.py
@@ -10,7 +10,8 @@ from multiprocessing import Pool
 from colorama import init, Fore
 from invoke import task, run
 import shutil
-from typing import Dict
+from typing import Dict, List
+import copy
 
 #######################################################
 # Working around for issue https://github.com/pyinvoke/invoke/issues/833 in python3.11
@@ -63,9 +64,11 @@ EMITTER_OPTIONS = {
     "type/model/usage": {
         "package-name": "typetest-model-usage",
     },
-    "type/model/visibility": {
-        "package-name": "typetest-model-visibility",
-    },
+    "type/model/visibility": [
+        {"package-name": "typetest-model-visibility"},
+        {"package-name": "headasbooleantrue", "head-as-boolean": "true"},
+        {"package-name": "headasbooleanfalse", "head-as-boolean": "false"},
+    ],
     "type/property/nullable": {
         "package-name": "typetest-property-nullable",
     },
@@ -80,22 +83,31 @@ EMITTER_OPTIONS = {
     },
 }
 
+def _default_output_dir(spec: Path) -> str:
+    return str(spec.relative_to(CADL_RANCH_DIR).as_posix()).replace("/", "-")
 
-def _get_emitter_option(spec: Path) -> Dict[str, str]:
+
+def _get_emitter_option(spec: Path) -> List[Dict[str, str]]:
     name = str(spec.relative_to(CADL_RANCH_DIR).as_posix())
-    return EMITTER_OPTIONS.get(name, {})
+    result = EMITTER_OPTIONS.get(name, [])
+    if isinstance(result, dict):
+        return [result]
+    return result
 
 
-def _add_options(spec: Path, debug=False) -> str:
-    options = {
-        "emitter-output-dir": f"{PLUGIN_DIR}/test/generated/{_get_package_name(spec)}"
-    }
+def _add_options(spec: Path, debug=False) -> List[str]:
     # if debug:
     #   options["debug"] = "true"
-    options.update(_get_emitter_option(spec))
-    return " --option ".join(
+    result = []
+    for config in _get_emitter_option(spec):
+        config_copy = copy.copy(config)
+        config_copy["emitter-output-dir"] = f"{PLUGIN_DIR}/test/generated/{config['package-name']}"
+        result.append(config_copy)
+    if not result:
+        result.append({"emitter-output-dir": _default_output_dir(spec)})
+    return [" --option ".join(
         [f"@azure-tools/typespec-python.{k}={v} " for k, v in options.items()]
-    )
+    ) for options in result]
 
 
 def _entry_file_name(path: Path) -> Path:
@@ -107,7 +119,7 @@ def _entry_file_name(path: Path) -> Path:
 def regenerate(c, name=None, debug=False):
     specs = [
         s for s in CADL_RANCH_DIR.glob("**/*")
-        if s.is_dir() and any(f for f in s.iterdir() if f.name == "main.tsp" and "authentication/http/custom" not in s.as_posix())
+        if s.is_dir() and any(f for f in s.iterdir() if f.name == "main.tsp")
     ]
     if name:
         specs = [s for s in specs if name.lower() in str(s)]
@@ -120,24 +132,23 @@ def regenerate(c, name=None, debug=False):
             ]
         )
     for spec in specs:
-        Path(f"{PLUGIN_DIR}/test/generated/{_get_package_name(spec)}").mkdir(
-            parents=True, exist_ok=True
-        )
+        for pacakge_name in _get_package_names(spec):
+            Path(f"{PLUGIN_DIR}/test/generated/{pacakge_name}").mkdir(
+                parents=True, exist_ok=True
+            )
     _run_cadl(
         [
-            f"tsp compile {_entry_file_name(spec)} --emit={PLUGIN_DIR} --option {_add_options(spec, debug)}"
-            for spec in specs
+            f"tsp compile {_entry_file_name(spec)} --emit={PLUGIN_DIR} --option {option}"
+            for spec in specs for option in _add_options(spec, debug)
         ]
     )
 
 
-def _get_package_name(spec: Path):
-    if _get_emitter_option(spec).get("package-name"):
-        return _get_emitter_option(spec)["package-name"]
-    return (
-        str(spec.relative_to(CADL_RANCH_DIR).as_posix())
-        .replace("/", "-")
-    )
+def _get_package_names(spec: Path) -> List[str]:
+    result = [config["package-name"] for config in _get_emitter_option(spec)]
+    if not result:
+        result.append(_default_output_dir(spec))
+    return result
 
 
 def _run_cadl(cmds):

packages/typespec-python/src/lib.ts
@@ -7,6 +7,7 @@ export interface PythonEmitterOptions {
     "output-dir"?: string;
     "package-mode"?: string;
     "package-pprint-name"?: string;
+    "head-as-boolean"?: boolean;
     "debug"?: boolean;
 }
 
@@ -20,6 +21,7 @@ const EmitterOptionsSchema: JSONSchemaType<PythonEmitterOptions> = {
         "output-dir": { type: "string", nullable: true },
         "package-mode": { type: "string", nullable: true },
         "package-pprint-name": { type: "string", nullable: true },
+        "head-as-boolean": { type: "boolean", nullable: true },
         "debug": { type: "boolean", nullable: true },
     },
     required: [],

packages/typespec-python/test/generated/headasbooleanfalse/headasbooleanfalse/__init__.py
@@ -1,0 +1,26 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# Code generated by Microsoft (R) Python Code Generator.
+# Changes may cause incorrect behavior and will be lost if the code is regenerated.
+# --------------------------------------------------------------------------
+
+from ._client import VisibilityClient
+from ._version import VERSION
+
+__version__ = VERSION
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+
+__all__ = [
+    "VisibilityClient",
+]
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()

packages/typespec-python/test/generated/headasbooleanfalse/headasbooleanfalse/_client.py
@@ -1,0 +1,62 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# Code generated by Microsoft (R) Python Code Generator.
+# Changes may cause incorrect behavior and will be lost if the code is regenerated.
+# --------------------------------------------------------------------------
+
+from copy import deepcopy
+from typing import Any
+
+from azure.core import PipelineClient
+from azure.core.rest import HttpRequest, HttpResponse
+
+from ._configuration import VisibilityClientConfiguration
+from ._operations import VisibilityClientOperationsMixin
+from ._serialization import Deserializer, Serializer
+
+
+class VisibilityClient(VisibilityClientOperationsMixin):  # pylint: disable=client-accepts-api-version-keyword
+    """Illustrates models with visibility properties."""
+
+    def __init__(self, **kwargs: Any) -> None:  # pylint: disable=missing-client-constructor-parameter-credential
+        _endpoint = "http://localhost:3000"
+        self._config = VisibilityClientConfiguration(**kwargs)
+        self._client: PipelineClient = PipelineClient(base_url=_endpoint, config=self._config, **kwargs)
+
+        self._serialize = Serializer()
+        self._deserialize = Deserializer()
+        self._serialize.client_side_validation = False
+
+    def send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+        """Runs the network request through the client's chained policies.
+
+        >>> from azure.core.rest import HttpRequest
+        >>> request = HttpRequest("GET", "https://www.example.org/")
+        <HttpRequest [GET], url: 'https://www.example.org/'>
+        >>> response = client.send_request(request)
+        <HttpResponse: 200 OK>
+
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
+
+        :param request: The network request you want to make. Required.
+        :type request: ~azure.core.rest.HttpRequest
+        :keyword bool stream: Whether the response payload will be streamed. Defaults to False.
+        :return: The response of your network call. Does not do error handling on your response.
+        :rtype: ~azure.core.rest.HttpResponse
+        """
+
+        request_copy = deepcopy(request)
+        request_copy.url = self._client.format_url(request_copy.url)
+        return self._client.send_request(request_copy, **kwargs)
+
+    def close(self) -> None:
+        self._client.close()
+
+    def __enter__(self) -> "VisibilityClient":
+        self._client.__enter__()
+        return self
+
+    def __exit__(self, *exc_details: Any) -> None:
+        self._client.__exit__(*exc_details)

packages/typespec-python/test/generated/headasbooleanfalse/headasbooleanfalse/_configuration.py
@@ -1,0 +1,39 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# Code generated by Microsoft (R) Python Code Generator.
+# Changes may cause incorrect behavior and will be lost if the code is regenerated.
+# --------------------------------------------------------------------------
+
+from typing import Any
+
+from azure.core.configuration import Configuration
+from azure.core.pipeline import policies
+
+from ._version import VERSION
+
+
+class VisibilityClientConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
+    """Configuration for VisibilityClient.
+
+    Note that all parameters used to create this instance are saved as instance
+    attributes.
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
+        super(VisibilityClientConfiguration, self).__init__(**kwargs)
+
+        kwargs.setdefault("sdk_moniker", "headasbooleanfalse/{}".format(VERSION))
+        self._configure(**kwargs)
+
+    def _configure(self, **kwargs: Any) -> None:
+        self.user_agent_policy = kwargs.get("user_agent_policy") or policies.UserAgentPolicy(**kwargs)
+        self.headers_policy = kwargs.get("headers_policy") or policies.HeadersPolicy(**kwargs)
+        self.proxy_policy = kwargs.get("proxy_policy") or policies.ProxyPolicy(**kwargs)
+        self.logging_policy = kwargs.get("logging_policy") or policies.NetworkTraceLoggingPolicy(**kwargs)
+        self.http_logging_policy = kwargs.get("http_logging_policy") or policies.HttpLoggingPolicy(**kwargs)
+        self.retry_policy = kwargs.get("retry_policy") or policies.RetryPolicy(**kwargs)
+        self.custom_hook_policy = kwargs.get("custom_hook_policy") or policies.CustomHookPolicy(**kwargs)
+        self.redirect_policy = kwargs.get("redirect_policy") or policies.RedirectPolicy(**kwargs)
+        self.authentication_policy = kwargs.get("authentication_policy")

packages/typespec-python/test/generated/headasbooleanfalse/headasbooleanfalse/_operations/__init__.py
@@ -1,0 +1,19 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# Code generated by Microsoft (R) Python Code Generator.
+# Changes may cause incorrect behavior and will be lost if the code is regenerated.
+# --------------------------------------------------------------------------
+
+from ._operations import VisibilityClientOperationsMixin
+
+from ._patch import __all__ as _patch_all
+from ._patch import *  # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
+
+__all__ = [
+    "VisibilityClientOperationsMixin",
+]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()

commit 7e89d18866ed63643ec5920de5afe60febf8fc8f

fix regeneration issue
introduced in https://github.com/Azure/autorest.python/pull/1957

packages/typespec-python/tasks.py
@@ -83,7 +83,8 @@ EMITTER_OPTIONS = {
     },
 }
 
-def _default_output_dir(spec: Path) -> str:
+
+def _default_package_name(spec: Path) -> str:
     return str(spec.relative_to(CADL_RANCH_DIR).as_posix()).replace("/", "-")
 
 
@@ -104,7 +105,7 @@ def _add_options(spec: Path, debug=False) -> List[str]:
         config_copy["emitter-output-dir"] = f"{PLUGIN_DIR}/test/generated/{config['package-name']}"
         result.append(config_copy)
     if not result:
-        result.append({"emitter-output-dir": _default_output_dir(spec)})
+        result.append({"emitter-output-dir": f"{PLUGIN_DIR}/test/generated/{_default_package_name(spec)}"})
     return [" --option ".join(
         [f"@azure-tools/typespec-python.{k}={v} " for k, v in options.items()]
     ) for options in result]
@@ -147,7 +148,7 @@ def regenerate(c, name=None, debug=False):
 def _get_package_names(spec: Path) -> List[str]:
     result = [config["package-name"] for config in _get_emitter_option(spec)]
     if not result:
-        result.append(_default_output_dir(spec))
+        result.append(_default_package_name(spec))
     return result
 
 