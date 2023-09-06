067d0a95aadb41a03a3402c6dd6a2c658177ec01
Yuchao Yan <yuchaoyan@microsoft.com>
2023-09-06 10:41:20 +0800

    bump cadl-ranch 0.20.0 (#2093)
    * inv regenerate with new cadl-ranch 0.20.0
    
    * add test case

---
+++ packages/typespec-python/test/mock_api_tests/asynctests/test_azure_client_generator_core_access_async.py
@@ -0,0 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import pytest
+from _specs_.azure.clientgenerator.core.access.aio import AccessClient
+
+# TODO
---
+++ packages/typespec-python/test/mock_api_tests/asynctests/test_azure_client_generator_core_usage_async.py
@@ -0,0 +1,23 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import pytest
+from _specs_.azure.clientgenerator.core.usage.aio import UsageClient
+from _specs_.azure.clientgenerator.core.usage import models
+
+
+@pytest.fixture
+async def client():
+    async with UsageClient() as client:
+        yield client
+
+
+@pytest.mark.asyncio
+async def test_input_to_input_output(client: UsageClient):
+    await client.input_to_input_output(models.InputModel(name="Madge"))
+
+@pytest.mark.asyncio
+async def test_output_to_input_output(client: UsageClient):
+    assert models.OutputModel(name="Madge") == await client.output_to_input_output()
