bc501b626f4c835f42825985c92adfe3537406db
msyyc <70930885+msyyc@users.noreply.github.com>
2023-09-06 10:04:02 +0800

    add test case

--- packages/typespec-python/test/mock_api_tests/test_typetest_union.py
+++ packages/typespec-python/test/mock_api_tests/test_typetest_union.py
@@ -5,7 +5,7 @@
 # --------------------------------------------------------------------------
 import pytest
 from typetest.union import UnionClient
-from typetest.union.models import ModelWithNamedUnionProperty, ModelWithSimpleUnionProperty, Model1, Model2
+from typetest.union import models
 
 @pytest.fixture
 def client():
@@ -13,13 +13,25 @@ def client():
         yield client
 
 def test_send_int(client):
-    client.send_int(ModelWithSimpleUnionProperty(simple_union=1))
+    client.send_int(models.ModelWithSimpleUnionProperty(simple_union=1))
 
 def test_send_int_array(client):
-    client.send_int_array(ModelWithSimpleUnionProperty(simple_union=[1, 2]))
+    client.send_int_array(models.ModelWithSimpleUnionProperty(simple_union=[1, 2]))
 
 def test_send_first_named_union_value(client):
-    client.send_first_named_union_value(ModelWithNamedUnionProperty(named_union=Model1(name="model1", prop1=1)))
+    client.send_first_named_union_value(models.ModelWithNamedUnionProperty(named_union=models.Model1(name="model1", prop1=1)))
 
 def test_send_second_named_union_value(client):
-    client.send_second_named_union_value(ModelWithNamedUnionProperty(named_union=Model2(name="model2", prop2=2)))
+    client.send_second_named_union_value(models.ModelWithNamedUnionProperty(named_union=models.Model2(name="model2", prop2=2)))
+
+def test_receive_string(client):
+    assert client.receive_string() == models.ModelWithSimpleUnionPropertyInResponse(simple_union="string")
+
+def test_receive_int_array(client):
+    assert client.receive_int_array() == models.ModelWithSimpleUnionPropertyInResponse(simple_union=[1, 2])
+
+def test_receive_first_named_union_value(client):
+    assert client.receive_first_named_union_value() == models.ModelWithNamedUnionPropertyInResponse(named_union=models.Model1(name="model1", prop1=1))
+
+def test_receive_second_named_union_value(client):
+    assert client.receive_second_named_union_value() == models.ModelWithNamedUnionPropertyInResponse(named_union=models.Model2(name="model2", prop2=2))
