453991909614ec2730b71af00cc48e456a4c193c
msyyc <70930885+msyyc@users.noreply.github.com>
2023-09-07 16:07:31 +0800

    refactor to clean duplicated code

--- eng/scripts/run_mypy.py
+++ eng/scripts/run_mypy.py
@@ -12,15 +12,11 @@ from subprocess import check_call, CalledProcessError
 import os
 import logging
 import sys
-from pathlib import Path
-import argparse
-from multiprocessing import Pool
+from util import run_check, AUTOREST_PACKAGE_DIR
 
 logging.getLogger().setLevel(logging.INFO)
 
-root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
-autorest_package_dir = Path(root_dir) / Path("packages/autorest.python")
-config_file_location = os.path.join(autorest_package_dir, "mypy.ini")
+config_file_location = os.path.join(AUTOREST_PACKAGE_DIR, "mypy.ini")
 
 
 def _single_dir_mypy(mod):
@@ -48,68 +44,4 @@ def _single_dir_mypy(mod):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Run mypy against target folder. Add a local custom plugin to the path prior to execution. "
-    )
-    parser.add_argument(
-        "-p",
-        "--package",
-        dest="package",
-        help="The specific which package to verify, autorest.python or typespec-python. Optional.",
-        required=False,
-        default="autorest.python",
-    )
-    parser.add_argument(
-        "-t",
-        "--test-folder",
-        dest="test_folder",
-        help="The test folder we're in. Can be 'azure', 'multiapi', or 'vanilla'",
-        required=True,
-    )
-    parser.add_argument(
-        "-g",
-        "--generator",
-        dest="generator",
-        help="The generator we're using. Can be 'legacy', 'version-tolerant'.",
-        required=False,
-    )
-    parser.add_argument(
-        "-f",
-        "--file-name",
-        dest="file_name",
-        help="The specific file name if you only want to run one file. Optional.",
-        required=False,
-    )
-    parser.add_argument(
-        "-s",
-        "--subfolder",
-        dest="subfolder",
-        help="The specific sub folder to validate, default to Expected/AcceptanceTests. Optional.",
-        required=False,
-        default="Expected/AcceptanceTests",
-    )
-
-    args = parser.parse_args()
-
-    pkg_dir = (
-        Path(root_dir)
-        / Path(f"packages/{args.package}")
-        / Path("test")
-        / Path(args.test_folder)
-    )
-    if args.generator:
-        pkg_dir /= Path(args.generator)
-    if args.subfolder:
-        pkg_dir /= Path(args.subfolder)
-    dirs = [d for d in pkg_dir.iterdir() if d.is_dir()]
-    if args.file_name:
-        dirs = [d for d in dirs if d.stem.lower() == args.file_name.lower()]
-    if len(dirs) > 1:
-        with Pool() as pool:
-            result = pool.map(_single_dir_mypy, dirs)
-        response = all(result)
-    else:
-        response = _single_dir_mypy(dirs[0])
-    if not response:
-        logging.error("Linting fails")
-        exit(1)
+    run_check("mypy", _single_dir_mypy, "MyPy")
