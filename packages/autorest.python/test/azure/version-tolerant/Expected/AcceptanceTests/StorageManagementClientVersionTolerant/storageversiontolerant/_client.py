085d40fc767db372c82e46cfa02212a0f692cc39
msyyc <70930885+msyyc@users.noreply.github.com>
2023-09-08 10:23:43 +0800

    inv reg

--- packages/autorest.python/test/azure/version-tolerant/Expected/AcceptanceTests/StorageManagementClientVersionTolerant/storageversiontolerant/_client.py
+++ packages/autorest.python/test/azure/version-tolerant/Expected/AcceptanceTests/StorageManagementClientVersionTolerant/storageversiontolerant/_client.py
@@ -33,8 +33,8 @@ class StorageManagementClient:  # pylint: disable=client-accepts-api-version-key
     :type subscription_id: str
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
-    :keyword endpoint: Service URL. Default value is "https://management.azure.com".
-    :paramtype endpoint: str
+    :param endpoint: Service URL. Default value is "https://management.azure.com".
+    :type endpoint: str
     :keyword api_version: Api Version. Default value is "2015-05-01-preview". Note that overriding
      this default value may result in unsupported behavior.
     :paramtype api_version: str
@@ -46,7 +46,6 @@ class StorageManagementClient:  # pylint: disable=client-accepts-api-version-key
         self,
         subscription_id: str,
         credential: "TokenCredential",
-        *,
         endpoint: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
