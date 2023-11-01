0b9534902476ea361c29811ec56e9f4cb9951b6e
msyyc <70930885+msyyc@users.noreply.github.com>
2023-11-01 10:26:05 +0800

    add async test case for unbreaded

--- packages/typespec-python/test/unbranded/mock_api_tests/asynctests/test_unbranded_async.py
+++ packages/typespec-python/test/unbranded/mock_api_tests/asynctests/test_unbranded_async.py
@@ -3,26 +3,21 @@
 # Licensed under the MIT License.
 # ------------------------------------
 import traceback
-from importlib import import_module
 import pytest
-from typetest.scalar import ScalarClient
+from typetest.scalar.aio import ScalarClient
 from corehttp.exceptions import HttpResponseError
 
 
 @pytest.fixture
-def client():
-    with ScalarClient() as client:
+async def client():
+    async with ScalarClient() as client:
         yield client
 
 
-def test_module():
-    with pytest.raises(ModuleNotFoundError):
-        import_module("azure")
-
-
-def test_track_back(client: ScalarClient):
+@pytest.mark.asyncio
+async def test_track_back(client: ScalarClient):
     try:
-        client.string.put("to raise exception")
+        await client.string.put("to raise exception")
     except HttpResponseError:
         track_back = traceback.format_exc().lower()
         assert "azure" not in track_back
