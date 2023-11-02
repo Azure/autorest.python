af5619f6192258d19ff21ad6412a7d3228b56710
Yuchao Yan <yuchaoyan@microsoft.com>
2023-11-02 11:14:45 +0800

    fix docstring for match_condition (#2232)

--- packages/typespec-python/test/unbranded/generated/special-headers-conditional-request/specialheaders/conditionalrequest/aio/_operations/_operations.py
+++ packages/typespec-python/test/unbranded/generated/special-headers-conditional-request/specialheaders/conditionalrequest/aio/_operations/_operations.py
@@ -41,7 +41,7 @@ class ConditionalRequestClientOperationsMixin(ConditionalRequestClientMixinABC):
          None.
         :paramtype etag: str
         :keyword match_condition: The match condition to use upon the etag. Default value is None.
-        :paramtype match_condition: ~corehttpMatchConditions
+        :paramtype match_condition: ~corehttp.MatchConditions
         :keyword bool stream: Whether to stream the response of this operation. Defaults to False. You
          will have to context manage the returned stream.
         :return: None
@@ -100,7 +100,7 @@ class ConditionalRequestClientOperationsMixin(ConditionalRequestClientMixinABC):
          None.
         :paramtype etag: str
         :keyword match_condition: The match condition to use upon the etag. Default value is None.
-        :paramtype match_condition: ~corehttpMatchConditions
+        :paramtype match_condition: ~corehttp.MatchConditions
         :keyword bool stream: Whether to stream the response of this operation. Defaults to False. You
          will have to context manage the returned stream.
         :return: None
