95a6cfd129d65d73f44bca5ee0967f994c8906bb
msyyc <70930885+msyyc@users.noreply.github.com>
2023-11-07 17:03:35 +0800

    update test

--- packages/typespec-python/test/azure/generated/typetest-property-optional/typetest/property/optional/test.py
+++ packages/typespec-python/test/azure/generated/typetest-property-optional/typetest/property/optional/test.py
@@ -1,6 +1,14 @@
 from typetest.property.optional.models import Test, TestProperties
 
 
+test = Test(properties=TestProperties(name="old"))
+assert test.name == "old"
+test = Test({"properties": {"name": "old"}})
+assert test.name == "old"
 
 test = Test(name="old")
 assert test.properties.name == "old"
+test.name = "new"
+assert test.properties.name == "new"
+test.properties.name = "newer"
+assert test.name == "newer"
