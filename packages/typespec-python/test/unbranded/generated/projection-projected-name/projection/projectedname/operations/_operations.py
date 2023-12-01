6f333fc252f5f64e1abeaeefc44a6278008ecc9d
iscai-msft <isabellavcai@gmail.com>
2023-12-01 14:16:43 -0500

    regen

--- packages/typespec-python/test/unbranded/generated/projection-projected-name/projection/projectedname/operations/_operations.py
+++ packages/typespec-python/test/unbranded/generated/projection-projected-name/projection/projectedname/operations/_operations.py
@@ -1,4 +1,4 @@
-# pylint: disable=too-many-lines
+# pylint: disable=too-many-lines,too-many-statements
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Unbranded Corporation. All rights reserved.
