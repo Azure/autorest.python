73d4e536eb8f21536b0073dc51f259a5b32c3b57
Yuchao Yan <yuchaoyan@microsoft.com>
2023-12-05 17:00:58 +0800

    Bump cadl ranch 0.27.1 (#2293)
    * dependency upgrade
    
    * add test case
    
    * add test case

--- packages/typespec-python/test/unbranded/generated/projection-projected-name/projection/projectedname/models/_models.py
+++ packages/typespec-python/test/unbranded/generated/projection-projected-name/projection/projectedname/models/_models.py
@@ -13,6 +13,37 @@ from .. import _model_base
 from .._model_base import rest_field
 
 
+class ClientModel(_model_base.Model):
+    """ClientModel.
+
+    All required parameters must be populated in order to send to server.
+
+    :ivar default_name: Pass in true. Required.
+    :vartype default_name: bool
+    """
+
+    default_name: bool = rest_field(name="defaultName")
+    """Pass in true. Required."""
+
+    @overload
+    def __init__(
+        self,
+        *,
+        default_name: bool,
+    ):
+        ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]):
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
+        super().__init__(*args, **kwargs)
+
+
 class ClientProjectedNameModel(_model_base.Model):
     """ClientProjectedNameModel.
 
@@ -123,6 +154,37 @@ class LanguageProjectedNameModel(_model_base.Model):
         self,
         *,
         python_name: bool,
+    ):
+        ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]):
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
+        super().__init__(*args, **kwargs)
+
+
+class PythonModel(_model_base.Model):
+    """PythonModel.
+
+    All required parameters must be populated in order to send to server.
+
+    :ivar default_name: Pass in true. Required.
+    :vartype default_name: bool
+    """
+
+    default_name: bool = rest_field(name="defaultName")
+    """Pass in true. Required."""
+
+    @overload
+    def __init__(
+        self,
+        *,
+        default_name: bool,
     ):
         ...
 
