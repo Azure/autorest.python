285f9cfad5bed585a8279e8f6aceb5d1658d8615
msyyc <70930885+msyyc@users.noreply.github.com>
2023-12-08 13:01:58 +0800

    fix ci

--- packages/autorest.python/autorest/preprocess/python_mappings.py
+++ packages/autorest.python/autorest/preprocess/python_mappings.py
@@ -163,7 +163,7 @@ RESERVED_WORDS = {
         *_always_reserved,
     ],
     PadType.MODEL: [*_always_reserved],
-    PadType.PROPERTY: ["self", *_always_reserved, "int", "float", "str", "bool", "bytes"],
+    PadType.PROPERTY: ["self", "int", *_always_reserved],
     PadType.ENUM: ["mro", *_always_reserved],
     PadType.OPERATION_GROUP: [*_always_reserved],
 }
