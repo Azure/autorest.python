a8ee91d96465900c7f5a053b9b839991147df303
Yuchao Yan <yuchaoyan@microsoft.com>
2024-01-30 17:30:44 +0800

    add test

--- packages/typespec-python/test/generic_mock_api_tests/test_projection_projected_name.py
+++ packages/typespec-python/test/generic_mock_api_tests/test_projection_projected_name.py
@@ -4,34 +4,34 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
-from projection.projectedname import ProjectedNameClient, models
+from projection.clientnameandencodedname import ClientNameAndEncodedNameClient, models
 
 @pytest.fixture
 def client():
-    with ProjectedNameClient() as client:
+    with ClientNameAndEncodedNameClient() as client:
         yield client
 
-def test_property_json(client: ProjectedNameClient):
-    client.property.json(models.JsonAndClientProjectedNameModel(client_name=True))
+def test_property_json(client: ClientNameAndEncodedNameClient):
+    client.property.json(models.JsonAndClientClientNameAndEncodedNameModel(client_name=True))
 
-def test_property_client(client: ProjectedNameClient):
-    client.property.client(models.ClientProjectedNameModel(client_name=True))
+def test_property_client(client: ClientNameAndEncodedNameClient):
+    client.property.client(models.ClientClientNameAndEncodedNameModel(client_name=True))
 
-def test_property_language(client: ProjectedNameClient):
-    client.property.language(models.LanguageProjectedNameModel(python_name=True))
+def test_property_language(client: ClientNameAndEncodedNameClient):
+    client.property.language(models.LanguageClientNameAndEncodedNameModel(python_name=True))
 
-def test_property_json_and_client(client: ProjectedNameClient):
-    client.property.json_and_client(models.JsonAndClientProjectedNameModel(client_name=True))
+def test_property_json_and_client(client: ClientNameAndEncodedNameClient):
+    client.property.json_and_client(models.JsonAndClientClientNameAndEncodedNameModel(client_name=True))
 
-def test_operation(client: ProjectedNameClient):
+def test_operation(client: ClientNameAndEncodedNameClient):
     # test that projected client name for operation is client_name
     client.client_name()
 
-def test_parameter(client: ProjectedNameClient):
+def test_parameter(client: ClientNameAndEncodedNameClient):
     client.parameter(client_name="true")
 
-def test_model_client(client: ProjectedNameClient):
+def test_model_client(client: ClientNameAndEncodedNameClient):
     client.model.client(models.ClientModel(default_name=True))
 
-def test_model_language(client: ProjectedNameClient):
+def test_model_language(client: ClientNameAndEncodedNameClient):
     client.model.language(models.PythonModel(default_name=True))
