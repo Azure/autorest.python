08e4c76b8b4045cbfd74b26a92b7653e3752e5a1
Krista Pratico <krpratic@microsoft.com>
2024-02-02 16:30:22 -0800

    fix mypy

--- packages/autorest.python/autorest/codegen/models/property.py
+++ packages/autorest.python/autorest/codegen/models/property.py
@@ -101,7 +101,7 @@ class Property(BaseModel):  # pylint: disable=too-many-instance-attributes
         """If this discriminator is on the base model for polymorphic inheritance"""
         if self.is_enum_discriminator:
             return self.is_polymorphic and self.client_default_value is None
-        return self.is_discriminator and self.is_polymorphic and cast(ConstantType, self.type.value) is None
+        return self.is_discriminator and self.is_polymorphic and cast(ConstantType, self.type).value is None
 
     def type_annotation(self, *, is_operation_file: bool = False) -> str:
         types_type_annotation = self.type.type_annotation(
