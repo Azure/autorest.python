9ebef135709539ac71aaa3c19635dbe3677dee28
Krista Pratico <krpratic@microsoft.com>
2024-02-07 12:30:31 -0800

    update version for next next pyright (#2351)

--- packages/typespec-python/test/azure/generated/azure-mgmt-spheredpg/setup.py
+++ packages/typespec-python/test/azure/generated/azure-mgmt-spheredpg/setup.py
@@ -65,6 +65,7 @@ setup(
     install_requires=[
         "isodate<1.0.0,>=0.6.1",
         "azure-mgmt-core<2.0.0,>=1.3.2",
+        "typing-extensions>=4.6.0",
     ],
     python_requires=">=3.8",
 )
