07bb846922b473bef557b7ad6cfa4ea7e9b6cd76
iscai-msft <isabellavcai@gmail.com>
2024-02-27 17:47:12 -0500

    regen with dev 14

--- packages/typespec-python/test/azure/generated/azure-core-traits/specs/azure/core/traits/models/_enums.py
+++ packages/typespec-python/test/azure/generated/azure-core-traits/specs/azure/core/traits/models/_enums.py
@@ -14,10 +14,4 @@ class RepeatabilityResult(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Repeatability Result header options."""
 
     ACCEPTED = "accepted"
-    """If the request was accepted and the server guarantees that the server state reflects a single
-    execution of the operation."""
     REJECTED = "rejected"
-    """If the request was rejected because the combination of Repeatability-First-Sent and
-    Repeatability-Request-ID were invalid
-    or because the Repeatability-First-Sent value was outside the range of values held by the
-    server."""
