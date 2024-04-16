d07b408ac5dd4b5e466685fcf4e7334997b8cec8
msyyc <70930885+msyyc@users.noreply.github.com>
2024-04-16 13:45:19 +0800

    enable file desc for test

--- packages/typespec-python/test/generic_mock_api_tests/unittests/test_model_init.py
+++ packages/typespec-python/test/generic_mock_api_tests/unittests/test_model_init.py
@@ -2,7 +2,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-from typetest.property.valuetypes.models import ModelProperty, InnerModel
+from typetest.property.valuetypes.models import ModelProperty, InnerModel, BytesProperty
+import io
+from pathlib import Path
+
+JPG = Path(__file__).parent.parent / "data/image.jpg"
 
 def test_model_init_remove_deepcopy():
     origin = {"property": InnerModel(property="hello")}
@@ -15,3 +19,8 @@ def test_model_init_remove_deepcopy():
     origin["property"]["property"] = "world"
     # After remove deepcopy for model init, value change of original input will also influence the model
     assert dpg_model["property"]["property"] == "world"
+
+def test_model_init_io():
+    # it is OK to use io when model init after remove deepcopy
+    with open(JPG, "rb") as file:
+        BytesProperty(property=file)
