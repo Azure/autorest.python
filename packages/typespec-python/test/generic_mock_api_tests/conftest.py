b3f5f213333c690ce19d2a5a8eb606e1f4d0503c
iscai-msft <isabellavcai@gmail.com>
2024-04-30 15:48:52 -0400

    try multiple coverage files check-coverage

--- packages/typespec-python/test/generic_mock_api_tests/conftest.py
+++ packages/typespec-python/test/generic_mock_api_tests/conftest.py
@@ -13,6 +13,27 @@ from typing import List
 
 FILE_FOLDER = Path(__file__).parent
 
+def start_server_process():
+    curr_dir = Path(os.path.dirname(__file__))
+    path = curr_dir / Path("../../node_modules/@azure-tools/cadl-ranch-specs")
+    os.chdir(path.resolve())
+    cmd = f"cadl-ranch serve ./http --coverageFile {curr_dir}/cadl-ranch-coverage.json"
+    if os.name == "nt":
+        return subprocess.Popen(cmd, shell=True)
+    return subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+
+def terminate_server_process(process):
+    if os.name == 'nt':
+        process.kill()
+    else:
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)  # Send the signal to all the process groups
+
+@pytest.fixture(scope="session", autouse=True)
+def testserver():
+    """Start cadl ranch mock api tests"""
+    server = start_server_process()
+    yield
+    terminate_server_process(server)
 
 """
 Use to disambiguate the core library we use
