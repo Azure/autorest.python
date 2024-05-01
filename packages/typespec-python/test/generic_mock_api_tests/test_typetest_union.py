cdf086a9687bac93685d1a649d909b3ee0d999b4
iscai-msft <isabellavcai@gmail.com>
2024-05-01 15:06:35 -0400

    remove parametrize from union tests

--- packages/typespec-python/test/generic_mock_api_tests/test_typetest_union.py
+++ packages/typespec-python/test/generic_mock_api_tests/test_typetest_union.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from typing import Any, Type
 import pytest
 from typetest.union import UnionClient
 from typetest.union import models
@@ -14,51 +13,58 @@ def client():
     with UnionClient() as client:
         yield client
 
+def test_enums_only(client: UnionClient):
+    value = models.EnumsOnlyCases(lr="right", ud="up")
+    assert client.enums_only.get() == {"prop": value}
+    client.enums_only.send(prop=value)
 
-@pytest.mark.parametrize(
-    "og_name,value,res_model_type",
-    [
-        ("strings_only", "b", models.GetResponse),
-        ("string_extensible", "custom", models.GetResponse1),
-        ("string_extensible_named", "custom", models.GetResponse2),
-        ("ints_only", 2, models.GetResponse3),
-        ("floats_only", 2.2, models.GetResponse4),
-        ("models_only", models.Cat(name="test"), models.GetResponse5),
-        (
-            "enums_only",
-            models.EnumsOnlyCases(lr="right", ud="up"),
-            models.GetResponse6,
-        ),
-        (
-            "string_and_array",
-            models.StringAndArrayCases(string="test", array=["test1", "test2"]),
-            models.GetResponse7,
-        ),
-        (
-            "mixed_literals",
-            models.MixedLiteralsCases(
-                string_literal="a",
-                int_literal=2,
-                float_literal=3.3,
-                boolean_literal=True,
-            ),
-            models.GetResponse8,
-        ),
-        (
-            "mixed_types",
-            models.MixedTypesCases(
-                model=models.Cat(name="test"),
-                literal="a",
-                int_property=2,
-                boolean=True,
-                array=[models.Cat(name="test"), "a", 2, True],
-            ),
-            models.GetResponse9,
-        ),
-    ],
-)
-def test_union(client: UnionClient, og_name: str, value: Any, res_model_type: Type):
-    og_group = getattr(client, og_name)
-    assert og_group.get() == res_model_type(prop=value)
-    og_group.send(prop=value)
-    og_group.send({"prop": value})
+def test_floats_only(client: UnionClient):
+    value = 2.2
+    assert client.floats_only.get() == {"prop": value}
+    client.floats_only.send(prop=value)
+
+def test_ints_only(client: UnionClient):
+    value = 2
+    assert client.ints_only.get() == {"prop": value}
+    client.ints_only.send(prop=value)
+
+def test_mixed_literals(client: UnionClient):
+    value = models.MixedLiteralsCases(string_literal="a", int_literal=2, float_literal=3.3, boolean_literal=True)
+    assert client.mixed_literals.get() == {"prop": value}
+    client.mixed_literals.send(prop=value)
+
+def test_mixed_types(client: UnionClient):
+    value = models.MixedTypesCases(
+        model=models.Cat(name="test"),
+        literal="a",
+        int_property=2,
+        boolean=True,
+        array=[models.Cat(name="test"), "a", 2, True],
+    )
+    assert client.mixed_types.get() == {"prop": value}
+    client.mixed_types.send(prop=value)
+
+def test_models_only(client: UnionClient):
+    value = models.Cat(name="test")
+    assert client.models_only.get() == {"prop": value}
+    client.models_only.send(prop=value)
+
+def test_string_and_array(client: UnionClient):
+    value = models.StringAndArrayCases(string="test", array=["test1", "test2"])
+    assert client.string_and_array.get() == {"prop": value}
+    client.string_and_array.send(prop=value)
+
+def test_string_extensible(client: UnionClient):
+    value = "custom"
+    assert client.string_extensible.get() == {"prop": value}
+    client.string_extensible.send(prop=value)
+
+def test_string_extensible_named(client: UnionClient):
+    value = "custom"
+    assert client.string_extensible_named.get() == {"prop": value}
+    client.string_extensible_named.send(prop=value)
+
+def test_strings_only(client: UnionClient):
+    value = "b"
+    assert client.strings_only.get() == {"prop": value}
+    client.strings_only.send(prop=value)
