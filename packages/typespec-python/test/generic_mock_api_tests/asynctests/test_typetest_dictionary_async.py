f8c51839861e0fa81ce5eba582f47a0557fd3bc9
iscai-msft <isabellavcai@gmail.com>
2024-05-01 15:15:23 -0400

    remove parametrize from dictionary tests

--- packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_dictionary_async.py
+++ packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_dictionary_async.py
@@ -13,29 +13,65 @@ async def client():
     async with DictionaryClient() as client:
         yield client
 
-@pytest.mark.parametrize(
-    "og_name,val", [
-        ("int32_value", {"k1": 1, "k2": 2}),
-        ("int64_value",  {"k1": 2**53-1, "k2": -(2**53-1)}),
-        ("boolean_value", {"k1": True, "k2": False}),
-        ("string_value", {"k1": "hello", "k2": ""}),
-        ("float32_value", {"k1": 43.125}),
-        ("datetime_value",  {"k1": isodate.parse_datetime("2022-08-26T18:38:00Z")}),
-        ("duration_value", {"k1": isodate.parse_duration("P123DT22H14M12.011S")}),
-        ("unknown_value", {"k1": 1, "k2": "hello", "k3": None}),
-        ("model_value", {
-            "k1": models.InnerModel(property="hello"),
-            "k2": models.InnerModel(property="world"),
-        }),
-        ("recursive_model_value", {
-            "k1": models.InnerModel(property="hello", children={}),
-            "k2": models.InnerModel(property="world", children={"k2.1": models.InnerModel(property="inner world")}),
-        }),
-        ("nullable_float_value", {"k1": 1.25, "k2": 0.5, "k3": None}),
-    ]
-)
 @pytest.mark.asyncio
-async def test_dictionary(client: DictionaryClient, og_name: str, val: dict):
-    og_group = getattr(client, og_name)
-    assert await og_group.get() == val
-    await og_group.put(val)
+async def test_boolean_value(client: DictionaryClient):
+    value = {"k1": True, "k2": False}
+    assert await client.boolean_value.get() == value
+    await client.boolean_value.put(value)
+
+@pytest.mark.asyncio
+async def test_datetime_value(client: DictionaryClient):
+    value = {"k1": isodate.parse_datetime("2022-08-26T18:38:00Z")}
+    assert await client.datetime_value.get() == value
+    await client.datetime_value.put(value)
+
+@pytest.mark.asyncio
+async def test_duration_value(client: DictionaryClient):
+    value = {"k1": isodate.parse_duration("P123DT22H14M12.011S")}
+    assert await client.duration_value.get() == value
+    await client.duration_value.put(value)
+
+@pytest.mark.asyncio
+async def test_float32_value(client: DictionaryClient):
+    value = {"k1": 43.125}
+    assert await client.float32_value.get() == value
+    await client.float32_value.put(value)
+
+@pytest.mark.asyncio
+async def test_int32_value(client: DictionaryClient):
+    value = {"k1": 1, "k2": 2}
+    assert await client.int32_value.get() == value
+    await client.int32_value.put(value)
+
+@pytest.mark.asyncio
+async def test_int64_value(client: DictionaryClient):
+    value = {"k1": 2**53 - 1, "k2": -(2**53 - 1)}
+    assert await client.int64_value.get() == value
+    await client.int64_value.put(value)
+
+@pytest.mark.asyncio
+async def test_model_value(client: DictionaryClient):
+    value = {
+        "k1": models.InnerModel(property="hello"),
+        "k2": models.InnerModel(property="world"),
+    }
+    assert await client.model_value.get() == value
+    await client.model_value.put(value)
+
+@pytest.mark.asyncio
+async def test_nullable_float_value(client: DictionaryClient):
+    value = {"k1": 1.25, "k2": 0.5, "k3": None}
+    assert await client.nullable_float_value.get() == value
+    await client.nullable_float_value.put(value)
+
+@pytest.mark.asyncio
+async def test_string_value(client: DictionaryClient):
+    value = {"k1": "hello", "k2": ""}
+    assert await client.string_value.get() == value
+    await client.string_value.put(value)
+
+@pytest.mark.asyncio
+async def test_unknown_value(client: DictionaryClient):
+    value = {"k1": 1, "k2": "hello", "k3": None}
+    assert await client.unknown_value.get() == value
+    await client.unknown_value.put(value)
--- packages/typespec-python/test/generic_mock_api_tests/test_typetest_dictionary.py
+++ packages/typespec-python/test/generic_mock_api_tests/test_typetest_dictionary.py
@@ -11,28 +11,55 @@ def client():
     with DictionaryClient() as client:
         yield client
 
-@pytest.mark.parametrize(
-    "og_name,val", [
-        ("int32_value", {"k1": 1, "k2": 2}),
-        ("int64_value",  {"k1": 2**53-1, "k2": -(2**53-1)}),
-        ("boolean_value", {"k1": True, "k2": False}),
-        ("string_value", {"k1": "hello", "k2": ""}),
-        ("float32_value", {"k1": 43.125}),
-        ("datetime_value",  {"k1": isodate.parse_datetime("2022-08-26T18:38:00Z")}),
-        ("duration_value", {"k1": isodate.parse_duration("P123DT22H14M12.011S")}),
-        ("unknown_value", {"k1": 1, "k2": "hello", "k3": None}),
-        ("model_value", {
-            "k1": models.InnerModel(property="hello"),
-            "k2": models.InnerModel(property="world"),
-        }),
-        ("recursive_model_value", {
-            "k1": models.InnerModel(property="hello", children={}),
-            "k2": models.InnerModel(property="world", children={"k2.1": models.InnerModel(property="inner world")}),
-        }),
-        ("nullable_float_value", {"k1": 1.25, "k2": 0.5, "k3": None}),
-    ]
-)
-def test_dictionary(client: DictionaryClient, og_name: str, val: dict):
-    og_group = getattr(client, og_name)
-    assert og_group.get() == val
-    og_group.put(val)
+def test_boolean_value(client: DictionaryClient):
+    value = {"k1": True, "k2": False}
+    assert client.boolean_value.get() == value
+    client.boolean_value.put(value)
+
+def test_datetime_value(client: DictionaryClient):
+    value = {"k1": isodate.parse_datetime("2022-08-26T18:38:00Z")}
+    assert client.datetime_value.get() == value
+    client.datetime_value.put(value)
+
+def test_duration_value(client: DictionaryClient):
+    value = {"k1": isodate.parse_duration("P123DT22H14M12.011S")}
+    assert client.duration_value.get() == value
+    client.duration_value.put(value)
+
+def test_float32_value(client: DictionaryClient):
+    value = {"k1": 43.125}
+    assert client.float32_value.get() == value
+    client.float32_value.put(value)
+
+def test_int32_value(client: DictionaryClient):
+    value = {"k1": 1, "k2": 2}
+    assert client.int32_value.get() == value
+    client.int32_value.put(value)
+
+def test_int64_value(client: DictionaryClient):
+    value = {"k1": 2**53 - 1, "k2": -(2**53 - 1)}
+    assert client.int64_value.get() == value
+    client.int64_value.put(value)
+
+def test_model_value(client: DictionaryClient):
+    value = {
+        "k1": models.InnerModel(property="hello"),
+        "k2": models.InnerModel(property="world"),
+    }
+    assert client.model_value.get() == value
+    client.model_value.put(value)
+
+def test_nullable_float_value(client: DictionaryClient):
+    value = {"k1": 1.25, "k2": 0.5, "k3": None}
+    assert client.nullable_float_value.get() == value
+    client.nullable_float_value.put(value)
+
+def test_string_value(client: DictionaryClient):
+    value = {"k1": "hello", "k2": ""}
+    assert client.string_value.get() == value
+    client.string_value.put(value)
+
+def test_unknown_value(client: DictionaryClient):
+    value = {"k1": 1, "k2": "hello", "k3": None}
+    assert client.unknown_value.get() == value
+    client.unknown_value.put(value)
