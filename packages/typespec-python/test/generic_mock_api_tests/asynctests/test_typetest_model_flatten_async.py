bf4556de85f9c37825d97a0dc97f5423803200f5
iscai-msft <isabellavcai@gmail.com>
2024-05-01 16:51:20 -0400

    remove usage from model flatten tests

--- packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_model_flatten_async.py
+++ packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_model_flatten_async.py
@@ -16,41 +16,94 @@ async def client():
 
 # ========== test for cadl-ranch ==========
 
-
-@pytest.mark.parametrize(
-    "op_name,reqs,reses",
-    [
-        (
-            "put_flatten_model",
-            [
-                FlattenModel(name="foo", properties=ChildModel(age=10, description="bar")),
-                FlattenModel(name="foo", age=10, description="bar"),
-            ],
-            [
-                FlattenModel(name="test", properties=ChildModel(age=1, description="test")),
-                FlattenModel(name="test", age=1, description="test"),
-            ],
-        ),
-        # python doesn't support nested flatten model
-        (
-            "put_nested_flatten_model",
-            [
-                NestedFlattenModel(
-                    name="foo",
-                    properties=ChildFlattenModel(summary="bar", properties=ChildModel(age=10, description="test")),
-                ),
-            ],
-            [
-                NestedFlattenModel(
-                    name="test",
-                    properties=ChildFlattenModel(summary="test", properties=ChildModel(age=1, description="foo")),
-                ),
-            ],
-        ),
-    ],
-)
 @pytest.mark.asyncio
-async def test_flatten(client: FlattenClient, op_name, reqs, reses):
-    for req in reqs:
-        for res in reses:
-            assert await getattr(client, op_name)(req) == res
+async def test_put_flatten_model(client: FlattenClient):
+    resp = FlattenModel(name="test", properties=ChildModel(age=1, description="test"))
+    assert await client.put_flatten_model(
+        FlattenModel(
+            name="foo",
+            properties=ChildModel(
+                age=10,
+                description="bar")
+            )
+        ) == resp
+    assert await client.put_flatten_model(
+        FlattenModel(
+            name="foo",
+            age=10,
+            description="bar"
+        )
+    ) == resp
+
+@pytest.mark.asyncio
+async def test_put_nested_flatten_model(client: FlattenClient):
+    # python doesn't support nested flatten model
+    assert await client.put_nested_flatten_model(
+        NestedFlattenModel(
+            name="foo",
+            properties=ChildFlattenModel(
+                summary="bar",
+                properties=ChildModel(
+                    age=10, description="test"
+                )
+            )
+        )
+    ) == NestedFlattenModel(
+        name="test",
+        properties=ChildFlattenModel(
+            summary="test",
+            properties=ChildModel(
+                age=1,
+                description="foo"
+            )
+        )
+    )
+                                           
+
+
+
+@pytest.mark.asyncio# ============test for compatibility ============
+async def test_dpg_model_common():
+    flatten_model = FlattenModel(name="hello", properties=ChildModel(age=0, description="test"))
+    assert flatten_model.name == "hello"
+    assert flatten_model.properties.age == 0
+    assert flatten_model.properties.description == "test"
+
+
+@pytest.mark.asyncio
+async def test_dpg_model_none():
+    flatten_model = FlattenModel()
+    assert flatten_model.name is None
+    assert flatten_model.properties is None
+    assert flatten_model.age is None
+    assert flatten_model.description is None
+
+
+@pytest.mark.asyncio
+async def test_dpg_model_compatility():
+    flatten_model = FlattenModel(description="test", age=0)
+    assert flatten_model.description == "test"
+    assert flatten_model.age == 0
+    assert flatten_model.properties.description == "test"
+    assert flatten_model.properties.age == 0
+
+
+@pytest.mark.asyncio
+async def test_dpg_model_setattr():
+    flatten_model = FlattenModel()
+
+    flatten_model.age = 0
+    assert flatten_model.properties.age == 0
+    flatten_model.description = "test"
+    assert flatten_model.properties.description == "test"
+
+    flatten_model.properties.age = 1
+    assert flatten_model.age == 1
+    flatten_model.properties.description = "test2"
+    assert flatten_model.description == "test2"
+
+
+@pytest.mark.asyncio
+async def test_dpg_model_exception():
+    with pytest.raises(AttributeError):
+        FlattenModel().no_prop
--- packages/typespec-python/test/generic_mock_api_tests/test_typetest_model_flatten.py
+++ packages/typespec-python/test/generic_mock_api_tests/test_typetest_model_flatten.py
@@ -15,45 +15,48 @@ def client():
 
 
 # ========== test for cadl-ranch ==========
-        
-    
 
-@pytest.mark.parametrize(
-    "op_name,reqs,reses",
-    [
-        (
-            "put_flatten_model",
-            [
-                FlattenModel(name="foo", properties=ChildModel(age=10, description="bar")),
-                FlattenModel(name="foo", age=10, description="bar"),
-            ],
-            [
-                FlattenModel(name="test", properties=ChildModel(age=1, description="test")),
-                FlattenModel(name="test", age=1, description="test"),
-            ],
-        ),
-        # python doesn't support nested flatten model
-        (
-            "put_nested_flatten_model",
-            [
-                NestedFlattenModel(
-                    name="foo",
-                    properties=ChildFlattenModel(summary="bar", properties=ChildModel(age=10, description="test")),
-                ),
-            ],
-            [
-                NestedFlattenModel(
-                    name="test",
-                    properties=ChildFlattenModel(summary="test", properties=ChildModel(age=1, description="foo")),
-                ),
-            ],
-        ),
-    ],
-)
-def test_flatten(client: FlattenClient, op_name, reqs, reses):
-    for req in reqs:
-        for res in reses:
-            assert getattr(client, op_name)(req) == res
+def test_put_flatten_model(client: FlattenClient):
+    resp = FlattenModel(name="test", properties=ChildModel(age=1, description="test"))
+    assert client.put_flatten_model(
+        FlattenModel(
+            name="foo",
+            properties=ChildModel(
+                age=10,
+                description="bar")
+            )
+        ) == resp
+    assert client.put_flatten_model(
+        FlattenModel(
+            name="foo",
+            age=10,
+            description="bar"
+        )
+    ) == resp
+
+def test_put_nested_flatten_model(client: FlattenClient):
+    # python doesn't support nested flatten model
+    assert client.put_nested_flatten_model(
+        NestedFlattenModel(
+            name="foo",
+            properties=ChildFlattenModel(
+                summary="bar",
+                properties=ChildModel(
+                    age=10, description="test"
+                )
+            )
+        )
+    ) == NestedFlattenModel(
+        name="test",
+        properties=ChildFlattenModel(
+            summary="test",
+            properties=ChildModel(
+                age=1,
+                description="foo"
+            )
+        )
+    )
+                                           
 
 
 # ============test for compatibility ============
