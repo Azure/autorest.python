3f14f8c8efe2e87bf349e789916fb5b6276e0741
iscai-msft <isabellavcai@gmail.com>
2024-05-06 17:22:04 -0400

    add back dict recursive model test

--- packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_dictionary_async.py
+++ packages/typespec-python/test/generic_mock_api_tests/asynctests/test_typetest_dictionary_async.py
@@ -73,6 +73,15 @@ async def test_nullable_float_value(client: DictionaryClient):
     assert await client.nullable_float_value.get() == value
     await client.nullable_float_value.put(value)
 
+@pytest.mark.asyncio
+async def test_recursive_model_value(client: DictionaryClient):
+    value = {
+        "k1": models.InnerModel(property="hello", children={}),
+        "k2": models.InnerModel(property="world", children={"k2.1": models.InnerModel(property="inner world")}),
+    }
+    assert await client.recursive_model_value.get() == value
+    await client.recursive_model_value.put(value)
+
 
 @pytest.mark.asyncio
 async def test_string_value(client: DictionaryClient):
--- packages/typespec-python/test/generic_mock_api_tests/test_typetest_dictionary.py
+++ packages/typespec-python/test/generic_mock_api_tests/test_typetest_dictionary.py
@@ -64,6 +64,14 @@ def test_nullable_float_value(client: DictionaryClient):
     assert client.nullable_float_value.get() == value
     client.nullable_float_value.put(value)
 
+def test_recursive_model_value(client: DictionaryClient):
+    value = {
+        "k1": models.InnerModel(property="hello", children={}),
+        "k2": models.InnerModel(property="world", children={"k2.1": models.InnerModel(property="inner world")}),
+    }
+    assert client.recursive_model_value.get() == value
+    client.recursive_model_value.put(value)
+
 
 def test_string_value(client: DictionaryClient):
     value = {"k1": "hello", "k2": ""}
