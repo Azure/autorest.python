823092f9cabbf670988e08c0551b0634cf14acf9
iscai-msft <isabellavcai@gmail.com>
2024-05-29 10:02:46 -0400

    temp

--- packages/typespec-python/scripts/post-install.js
+++ packages/typespec-python/scripts/post-install.js
@@ -3,6 +3,7 @@ import {join, dirname} from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
+console.warn(__dirname);
 
 // Define the source and destination directories
 const sourceDir = join(__dirname, "..", "..", 'pygen');
