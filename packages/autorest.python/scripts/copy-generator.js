9c3499e3b80e81b4f4de7f8fdd144c9334aed602
tadelesh <tadelesh.shi@live.cn>
2024-07-03 11:06:09 +0800

    update script

--- packages/autorest.python/scripts/copy-generator.js
+++ packages/autorest.python/scripts/copy-generator.js
@@ -1,6 +1,5 @@
 const fs = require('fs-extra');
 const path = require('path');
-const url = require('url');
 
 // Define the source and destination directories
 const sourceDir = path.join(__dirname, "..", "node_modules", "@azure-tools", "typespec-python", "generator");
