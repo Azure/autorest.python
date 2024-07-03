75ef50fbb64dc73f2bda309e8b033b32b03fae7f
tadelesh <tadelesh.shi@live.cn>
2024-07-03 13:41:07 +0800

    update

--- packages/autorest.python/scripts/copy-generator.js
+++ packages/autorest.python/scripts/copy-generator.js
@@ -1,14 +1,46 @@
-const fs = require('fs-extra');
-const path = require('path');
+const { realpath, stat, readFile } = require("fs/promises");
+const fs = require("fs-extra");
+const path = require("path");
 
-// Define the source and destination directories
-const sourceDir = path.join(__dirname, "..", "node_modules", "@azure-tools", "typespec-python", "generator");
-const destDir = path.join(__dirname, "..", "generator");
+async function resolveAndCopy() {
+    let resolved;
 
-// Delete the destination directory if it exists
-if (fs.existsSync(destDir)) {
-    fs.removeSync(destDir);
+    // Resolve @azure-tools/typespec-python module path
+    try {
+        const module = await import("@azure-tools/typespec-python");
+
+        const host = {
+            realpath,
+            readFile: async (path) => await readFile(path, "utf-8"),
+            stat,
+        };
+
+        resolved = await module.resolveModule(host, "@azure-tools/typespec-python", {
+            baseDir: process.cwd(),
+        });
+        if (resolved.type !== "module") {
+            throw new Error(
+                `Error resolving "@azure-tools/typespec-python", expected to find a node module but found a file: "${resolved.path}".`
+            );
+        }
+    } catch (err) {
+        throw new Error(
+            `Error resolving "@azure-tools/typespec-python", could not find the module: "${err}".`
+        );
+    }
+
+    // Define the source and destination directories
+    const sourceDir = path.join(resolved.path, "generator");
+    const destDir = path.join(__dirname, "..", "generator");
+
+    // Delete the destination directory if it exists
+    if (fs.existsSync(destDir)) {
+        fs.removeSync(destDir);
+    }
+
+    // Copy the source directory to the destination directory
+    fs.copySync(sourceDir, destDir);
 }
 
-// Copy the source directory to the destination directory
-fs.copySync(sourceDir, destDir);
+
+resolveAndCopy().catch(console.error);
--- packages/typespec-python/src/index.ts
+++ packages/typespec-python/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./emitter.js";
 export { $lib } from "./lib.js";
+export { resolveModule } from "@typespec/compiler";
