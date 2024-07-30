d0d588a62bcf4d9582945f152fee600dd5eb009c
iscai-msft <isabellavcai@gmail.com>
2024-07-30 17:49:23 -0400

    add script for regenerating

--- packages/typespec-python/scripts/regenerate.ts
+++ packages/typespec-python/scripts/regenerate.ts
@@ -2,7 +2,7 @@ import { exec as execCallback } from 'child_process';
 import { promisify } from 'util';
 import yargs from 'yargs';
 import { hideBin } from "yargs/helpers";
-import { join, resolve } from 'path';
+import { dirname, join, relative, resolve } from 'path';
 import { lstatSync, readdirSync, promises, access } from "fs";
 import { sync } from "glob";
 import { fileURLToPath } from 'url';
@@ -11,74 +11,216 @@ import { fileURLToPath } from 'url';
 const exec = promisify(execCallback);
 
 // Get the directory of the current file
-const PLUGIN_DIR = fileURLToPath(import.meta.url);
+const PLUGIN_DIR = resolve(fileURLToPath(import.meta.url), '../../../');
+const CADL_RANCH_DIR = resolve(PLUGIN_DIR, 'node_modules/@azure-tools/cadl-ranch-specs/http');
 
-// Function to execute CLI commands asynchronously
-async function executeCommand(command: string): Promise<string> {
-  try {
-    const { stdout, stderr } = await exec(command, { cwd: PLUGIN_DIR });
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      throw new Error(stderr);
-    }
-    return stdout;
-  } catch (error) {
-    console.error(`Error`);
-    throw error;
-  }
+const EMITTER_OPTIONS: Record<string, Record<string, string> | Record<string, string>[]> = {
+    "resiliency/srv-driven/old.tsp": {
+        "package-name": "resiliency-srv-driven1",
+        "package-mode": "azure-dataplane",
+        "package-pprint-name": "ResiliencySrvDriven1",
+    },
+    "resiliency/srv-driven": {
+        "package-name": "resiliency-srv-driven2",
+        "package-mode": "azure-dataplane",
+        "package-pprint-name": "ResiliencySrvDriven2",
+    },
+    "authentication/http/custom": {
+        "package-name": "authentication-http-custom",
+    },
+    "authentication/union": {
+        "package-name": "authentication-union",
+    },
+    "type/array": {
+        "package-name": "typetest-array",
+    },
+    "type/dictionary": {
+        "package-name": "typetest-dictionary",
+    },
+    "type/enum/extensible": {
+        "package-name": "typetest-enum-extensible",
+    },
+    "type/enum/fixed": {
+        "package-name": "typetest-enum-fixed",
+    },
+    "type/model/empty": {
+        "package-name": "typetest-model-empty",
+    },
+    "type/model/flatten": {
+        "package-name": "typetest-model-flatten",
+    },
+    "type/model/inheritance/enum-discriminator": {
+        "package-name": "typetest-model-enumdiscriminator",
+    },
+    "type/model/inheritance/nested-discriminator": {
+        "package-name": "typetest-model-nesteddiscriminator",
+    },
+    "type/model/inheritance/not-discriminated": {
+        "package-name": "typetest-model-notdiscriminated",
+    },
+    "type/model/inheritance/single-discriminator": {
+        "package-name": "typetest-model-singlediscriminator",
+    },
+    "type/model/inheritance/recursive": {
+        "package-name": "typetest-model-recursive",
+    },
+    "type/model/usage": {
+        "package-name": "typetest-model-usage",
+    },
+    "type/model/visibility": [
+        {"package-name": "typetest-model-visibility"},
+        {"package-name": "headasbooleantrue", "head-as-boolean": "true"},
+        {"package-name": "headasbooleanfalse", "head-as-boolean": "false"},
+    ],
+    "type/property/nullable": {
+        "package-name": "typetest-property-nullable",
+    },
+    "type/property/optionality": {
+        "package-name": "typetest-property-optional",
+    },
+    "type/property/additional-properties": {
+        "package-name": "typetest-property-additionalproperties",
+    },
+    "type/scalar": {
+        "package-name": "typetest-scalar",
+    },
+    "type/property/value-types": {
+        "package-name": "typetest-property-valuetypes",
+    },
+    "type/union": {
+        "package-name": "typetest-union",
+    },
+    "azure/core/lro/rpc-legacy": {
+        "package-name": "azurecore-lro-rpclegacy",
+    },
+    "azure/core/lro/rpc": {
+        "package-name": "azurecore-lro-rpc",
+    },
+    "client/structure/multi-client": {
+        "package-name": "client-structure-multiclient",
+    },
+    "client/structure/renamed-operation": {
+        "package-name": "client-structure-renamedoperation",
+    },
+    "client/structure/two-operation-group": {
+        "package-name": "client-structure-twooperationgroup",
+    },
+    "mgmt/sphere": [
+        {"package-name": "azure-mgmt-spheredpg"},
+    ],
 }
 
-interface RegenerateFlags {
+function getEmitterOption(spec: string, flags: RegenerateFlags): Record<string, string>[] {
+    const result = EMITTER_OPTIONS[spec] || [];
+    return Array.isArray(result) ? result : [result];
+
+}
+
+// Function to execute CLI commands asynchronously
+async function executeCommand(command: string): Promise<void> {
+    try {
+        const { stdout, stderr } = await exec(command);
+        if (stdout) console.log(`stdout: ${stdout}`);
+        if (stderr) console.error(`stderr: ${stderr}`);
+    } catch (error) {
+        console.error(`exec error: ${error}`);
+        throw error;
+    }
+}
+
+interface RegenerateFlagsInput {
     flavor?: "azure" | "unbranded";
     debug?: boolean;
     name?: string;
 }
 
-// function pathExists(path: string): boolean {
-//     try {
-//       access(path);
-//       return true;
-//     } catch {
-//       return false;
-//     }
-//   }
-
-  async function getSubdirectories(directory: string): Promise<string[]> {
-    const subdirectories: string[] = [];
-    const items = await promises.readdir(directory);
-  
-    for (const item of items) {
-      const itemPath = join(directory, item);
-      const stats = await promises.lstat(itemPath);
-  
-      if (stats.isDirectory()) {
-        subdirectories.push(itemPath);
-        console.log(itemPath)
-      }
-    }
-  
-    return subdirectories;
-  }
-
-async function someAsyncOperation(subdirectory: string): Promise<void> {
-    // Perform some asynchronous operation here
-    console.log(subdirectory);
+interface RegenerateFlags {
+    flavor: "azure" | "unbranded";
+    debug: boolean;
+    name?: string;
 }
 
-async function regenerate(flags: RegenerateFlags): Promise<boolean> {
+async function getSubdirectories(baseDir: string): Promise<string[]> {
+    const subdirectories: string[] = [];
+
+    async function searchDir(currentDir: string) {
+        const items = await promises.readdir(currentDir, { withFileTypes: true });
+
+        const promisesArray = items.map(async (item) => {
+            if (item.isDirectory()) {
+                const subDirPath = join(currentDir, item.name);
+                const mainTspPath = join(subDirPath, 'main.tsp');
+                const clientTspPath = join(subDirPath, 'client.tsp');
+
+                const hasMainTsp = await promises.access(mainTspPath).then(() => true).catch(() => false);
+                const hasClientTsp = await promises.access(clientTspPath).then(() => true).catch(() => false);
+
+                if (hasClientTsp) {
+                    subdirectories.push(resolve(subDirPath, "client.tsp"));
+                } else if (hasMainTsp) {
+                    subdirectories.push(resolve(subDirPath, "main.tsp"));
+                }
+
+                // Recursively search in the subdirectory
+                await searchDir(subDirPath);
+            }
+        });
+
+        await Promise.all(promisesArray);
+    }
+
+    await searchDir(baseDir);
+    return subdirectories;
+}
+
+function defaultPackageName(spec: string): string {
+    return relative(CADL_RANCH_DIR, dirname(spec)).replace(/\//g, "-").toLowerCase();
+}
+
+function addOptions(
+    spec: string,
+    generatedFolder: string,
+    flags: RegenerateFlags
+): string[] {
+    let options: Record<string, string> ={};
+    for (const config of getEmitterOption(spec, flags)) {
+        options = Object.assign(options, config);
+    }
+    options["flavor"] = flags.flavor;
+    if (options["emitter-output-dir"] === undefined) {
+        options["emitter-output-dir"] = `${generatedFolder}/test/${flags.flavor}/generated/${defaultPackageName(spec)}`;
+    }
+    if (flags.debug) {
+        options["debug"] = "true";
+    }
+    const emitterConfigs = Object.entries(options).flatMap(([k, v]) => {
+        return `--option @azure-tools/typespec-python.${k}=${v}`;
+    })
+    return emitterConfigs;
+}
+
+async function _regenerateSingle(spec: string, flags: RegenerateFlags): Promise<void> {
+    // Perform some asynchronous operation here
+    const options = addOptions(spec, PLUGIN_DIR, flags);
+    const command = `tsp compile ${spec} --emit=${PLUGIN_DIR} ${options.join(" ")}`;
+    console.log(command);
+    await executeCommand(command);
+}
+
+async function regenerate(flags: RegenerateFlagsInput): Promise<boolean> {
     if (flags.flavor === undefined) {
-        const azureGeneration = await regenerate({...flags, flavor: "azure"});
-        const unbrandedGeneration = await regenerate({...flags, flavor: "unbranded"});
+        const azureGeneration = await regenerate({...flags, flavor: "azure" });
+        const unbrandedGeneration = await regenerate({...flags, flavor: "unbranded" });
         return azureGeneration && unbrandedGeneration;
     } else {
-        const CADL_RANCH_DIR = resolve(PLUGIN_DIR, '../../../', 'node_modules/@azure-tools/cadl-ranch-specs/http');
+        const flagsDefault: RegenerateFlags = {debug: false, flavor: flags.flavor};
+        const CADL_RANCH_DIR = resolve(PLUGIN_DIR, 'node_modules/@azure-tools/cadl-ranch-specs/http');
         const subdirectories = await getSubdirectories(CADL_RANCH_DIR);
-            const promises = subdirectories.map(async (subdirectory) => {
-                // Perform additional asynchronous operations on each subdirectory here
-                await someAsyncOperation(subdirectory);
+        const promises = subdirectories.map(async (subdirectory) => {
+            // Perform additional asynchronous operations on each subdirectory here
+            await _regenerateSingle(subdirectory, { ...flagsDefault, ...flags });
         });
         await Promise.all(promises);
-        console.log("done");
         return true;
     }
 };
