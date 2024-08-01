9d033712517f2118c22a248e494a1c729f25855d
msyyc <70930885+msyyc@users.noreply.github.com>
2024-08-01 14:48:12 +0800

    review

--- packages/typespec-python/generator/pygen/codegen/models/code_model.py
+++ packages/typespec-python/generator/pygen/codegen/models/code_model.py
@@ -162,9 +162,7 @@ class CodeModel:  # pylint: disable=too-many-public-methods, disable=too-many-in
         """All of the model types in this class"""
         if not self._model_types:
             self._model_types = [
-                t
-                for t in self.types_map.values()
-                if isinstance(t, ModelType) and t.usage != UsageFlags.Default.value
+                t for t in self.types_map.values() if isinstance(t, ModelType) and t.usage != UsageFlags.Default.value
             ]
         return self._model_types
 
