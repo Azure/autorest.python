b7ecc12a3c69c4f4f0980d189d94f1dc42a8ac4b
iscai-msft <isabellavcai@gmail.com>
2024-08-15 16:07:03 -0400

    format

--- packages/autorest.python/scripts/run-tests.ts
+++ packages/autorest.python/scripts/run-tests.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-import { execSync } from 'child_process';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { execSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 interface Arguments {
     folder?: string;
     command?: string;
-};
+}
 
 const validFolders = [
     "azure/legacy",
@@ -14,51 +14,43 @@ const validFolders = [
     "vanilla/legacy",
     "vanilla/version-tolerant",
     "dpg/version-tolerant",
-]
-
-const validCommands = [
-    "ci",
-    "lint",
-    "mypy",
-    "pyright",
-    "apiview"
 ];
 
+const validCommands = ["ci", "lint", "mypy", "pyright", "apiview"];
 
 // Parse command-line arguments using yargs
 const argv = yargs(hideBin(process.argv))
-  .option('folder', {
-    alias: 'f',
-    describe: 'Specify the folder to use',
-    choices: validFolders,
-    type: 'string'
-  })
-  .option('command', {
-    alias: 'c',
-    describe: 'Specify the command to run',
-    choices: validCommands,
-    type: 'string'
-  }
-  ).argv as Arguments;
+    .option("folder", {
+        alias: "f",
+        describe: "Specify the folder to use",
+        choices: validFolders,
+        type: "string",
+    })
+    .option("command", {
+        alias: "c",
+        describe: "Specify the command to run",
+        choices: validCommands,
+        type: "string",
+    }).argv as Arguments;
 
 const foldersToProcess = argv.folder ? [argv.folder] : validFolders;
 
-const commandToRun = argv.command || 'all';
+const commandToRun = argv.command || "all";
 
 function getCommand(command: string, folder: string) {
     return `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e ${command}`;
 }
 
-foldersToProcess.forEach(folder => {
+foldersToProcess.forEach((folder) => {
     try {
-        if (commandToRun === 'all') {
+        if (commandToRun === "all") {
             for (const key in validCommands) {
                 console.log(`Running ${key} for folder ${folder}...`);
-                execSync(getCommand(key, folder), { stdio: 'inherit' });
+                execSync(getCommand(key, folder), { stdio: "inherit" });
             }
         } else if (getCommand(commandToRun, folder)) {
             console.log(`Running ${commandToRun} for folder ${folder}...`);
-            execSync(getCommand(commandToRun, folder), { stdio: 'inherit' });
+            execSync(getCommand(commandToRun, folder), { stdio: "inherit" });
         } else {
             console.error(`Error: Unknown command '${commandToRun}'.`);
             process.exit(1);
--- packages/typespec-python/scripts/run-tests.ts
+++ packages/typespec-python/scripts/run-tests.ts
@@ -1,52 +1,50 @@
 /* eslint-disable no-console */
-import { execSync } from 'child_process';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { execSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 interface Arguments {
     folder: string;
     command?: string;
-};
+}
 const validCommands = ["ci", "lint", "mypy", "pyright", "apiview"];
-
 
 // Parse command-line arguments using yargs
 const argv = yargs(hideBin(process.argv))
-  .option('folder', {
-    alias: 'f',
-    describe: 'Specify the folder to use',
-    choices: ['azure', 'unbranded'],
-    demandOption: true,
-    type: 'string'
-  })
-  .option('command', {
-    alias: 'c',
-    describe: 'Specify the command to run',
-    choices: validCommands,
-    type: 'string'
-  }
-  ).argv as Arguments;
+    .option("folder", {
+        alias: "f",
+        describe: "Specify the folder to use",
+        choices: ["azure", "unbranded"],
+        demandOption: true,
+        type: "string",
+    })
+    .option("command", {
+        alias: "c",
+        describe: "Specify the command to run",
+        choices: validCommands,
+        type: "string",
+    }).argv as Arguments;
 
 const folder = argv.folder;
-const commandToRun = argv.command || 'all';
+const commandToRun = argv.command || "all";
 
 const commands: Record<string, string> = {
-  ci: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e ci`,
-  lint: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e lint`,
-  mypy: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e mypy`,
-  pyright: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e pyright`,
-  apiview: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e apiview`
+    ci: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e ci`,
+    lint: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e lint`,
+    mypy: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e mypy`,
+    pyright: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e pyright`,
+    apiview: `FOLDER=${folder} tox -c ./test/${folder}/tox.ini -e apiview`,
 };
 
 try {
-    if (commandToRun === 'all') {
+    if (commandToRun === "all") {
         for (const key in commands) {
             console.log(`Running ${key}...`);
-            execSync(commands[key], { stdio: 'inherit' });
+            execSync(commands[key], { stdio: "inherit" });
         }
     } else if (commands[commandToRun]) {
         console.log(`Running ${commandToRun}...`);
-        execSync(commands[commandToRun], { stdio: 'inherit' });
+        execSync(commands[commandToRun], { stdio: "inherit" });
     } else {
         console.error(`Error: Unknown command '${commandToRun}'.`);
         process.exit(1);
