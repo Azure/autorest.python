4864f978cbd41e58b7bf29956cc2d51e6a3f3947
msyyc <70930885+msyyc@users.noreply.github.com>
2024-08-23 15:12:54 +0800

    fix nightly build

--- packages/typespec-python/scripts/eng/format.ts
+++ packages/typespec-python/scripts/eng/format.ts
@@ -1,16 +1,5 @@
 /* eslint-disable no-console */
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+
 import { runCommand } from "./utils.js";
 
-interface Arguments {
-    folderName: string;
-}
-
-// PARSE INPUT ARGUMENTS
-const argv = yargs(hideBin(process.argv)).option("folderName", {
-    type: "string",
-    description: "Specify the flavor",
-}).argv as Arguments;
-
-runCommand(`black ${argv.folderName.split(",").join(" ")}`, "black");
+runCommand("black .", "black");
