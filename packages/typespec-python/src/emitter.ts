b8e24ca63f20fa8fc1ee2b551ff5eb7a393dde1c
iscai-msft <isabellavcai@gmail.com>
2024-10-07 13:51:55 -0400

    add src

--- packages/typespec-python/src/emitter.ts
+++ packages/typespec-python/src/emitter.ts
@@ -3,5 +3,5 @@ import { PythonEmitterOptions } from "./lib.js";
 import { $onEmit as httpClientPythonOnEmit } from "@typespec/http-client-python";
 
 export async function $onEmit(context: EmitContext<PythonEmitterOptions>) {
-    httpClientPythonOnEmit(context);
+    await httpClientPythonOnEmit(context);
 }
