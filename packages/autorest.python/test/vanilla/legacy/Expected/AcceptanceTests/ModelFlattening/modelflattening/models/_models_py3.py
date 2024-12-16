05c48dfe4e779eb45288402fb9c1bad534e706f2
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2024-12-16 06:29:04 +0000

    Regenerate for autorest.python (2024-12-16 06:29:04)

--- packages/autorest.python/test/vanilla/legacy/Expected/AcceptanceTests/ModelFlattening/modelflattening/models/_models_py3.py
+++ packages/autorest.python/test/vanilla/legacy/Expected/AcceptanceTests/ModelFlattening/modelflattening/models/_models_py3.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Any, Dict, List, Literal, Optional, TYPE_CHECKING, Union
+from typing import Any, Dict, List, Literal, Optional, TYPE_CHECKING
 
 from .. import _serialization
 
