720f248dc261f25027c9660fe3cf7450ef4e6c19
Yuchao Yan <yuchaoyan@microsoft.com>
2025-01-02 15:45:54 +0800

    migration from cadl-ranch to spector (#2977)
    * update cadl-ranch to spector
    
    * update ignore
    
    * Update dependencies (2024-12-18 06:19:16)
    
    * Regenerate for typespec-python (2024-12-18 06:23:49)
    
    * fix ci
    
    * Fix ci
    
    * review
    
    * update package.json
    
    * add changelog
    
    * fix sub name in ci
    
    * Rename subdirectory variables for clarity
    
    * update internal-ci with specific coverage file name
    
    * update internal-ci for spector coverage upload
    
    * fix ci
    
    ---------
    
    Co-authored-by: AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
    Co-authored-by: Chenjie Shi <tadelesh.shi@live.cn>

--- packages/typespec-python/test/azure/generated/payload-pageable/payload/pageable/models/__init__.py
+++ packages/typespec-python/test/azure/generated/payload-pageable/payload/pageable/models/__init__.py
@@ -14,14 +14,18 @@ if TYPE_CHECKING:
 
 
 from ._models import (  # type: ignore
-    User,
+    LinkResponse,
+    LinkResponseLinks,
+    Pet,
 )
 from ._patch import __all__ as _patch_all
 from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "User",
+    "LinkResponse",
+    "LinkResponseLinks",
+    "Pet",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
 _patch_sdk()
