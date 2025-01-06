08b2db27799b5970bfb0076647cbcd02c709d91e
tadelesh <tadelesh.shi@live.cn>
2025-01-06 13:39:38 +0800

    set flavor to azure if not set

--- packages/typespec-python/src/emitter.ts
+++ packages/typespec-python/src/emitter.ts
@@ -3,5 +3,9 @@ import { PythonEmitterOptions } from "./lib.js";
 import { $onEmit as httpClientPythonOnEmit } from "@typespec/http-client-python";
 
 export async function $onEmit(context: EmitContext<PythonEmitterOptions>) {
+    // set flavor to azure if not set for python azure emitter
+    if (context.options.flavor === undefined) {
+        context.options.flavor = "azure";
+    }
     await httpClientPythonOnEmit(context);
 }
