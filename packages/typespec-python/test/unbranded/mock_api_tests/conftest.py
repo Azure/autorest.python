05cefd0855e608b5a1e50294a39baea90dae5fae
Yuchao Yan <yuchaoyan@microsoft.com>
2025-01-14 16:36:16 +0800

    fix ci

--- packages/typespec-python/test/unbranded/mock_api_tests/conftest.py
+++ packages/typespec-python/test/unbranded/mock_api_tests/conftest.py
@@ -10,6 +10,7 @@ import pytest
 import re
 from pathlib import Path
 
+FILE_FOLDER = Path(__file__).parent
 
 def start_server_process():
     http_path = Path(os.path.dirname(__file__)) / Path("../../../node_modules/@typespec/http-specs")
@@ -33,3 +34,57 @@ def testserver():
     server = start_server_process()
     yield
     terminate_server_process(server)
+
+
+SPECIAL_WORDS = [
+    "and",
+    "as",
+    "assert",
+    "async",
+    "await",
+    "break",
+    "class",
+    "constructor",
+    "continue",
+    "def",
+    "del",
+    "elif",
+    "else",
+    "except",
+    "exec",
+    "finally",
+    "for",
+    "from",
+    "global",
+    "if",
+    "import",
+    "in",
+    "is",
+    "lambda",
+    "not",
+    "or",
+    "pass",
+    "raise",
+    "return",
+    "try",
+    "while",
+    "with",
+    "yield",
+]
+
+
+@pytest.fixture
+def special_words() -> List[str]:
+    return SPECIAL_WORDS
+
+
+@pytest.fixture
+def png_data() -> bytes:
+    with open(str(FILE_FOLDER / "data/image.png"), "rb") as file_in:
+        return file_in.read()
+
+
+@pytest.fixture
+def jpg_data() -> bytes:
+    with open(str(FILE_FOLDER / "data/image.jpg"), "rb") as file_in:
+        return file_in.read()
