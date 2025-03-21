10817b20588685b1a15013987887b6abfb5abadc
tadelesh <tadelesh.shi@live.cn>
2025-03-21 21:18:56 +0800

    update options for azure emitter

--- packages/typespec-python/src/emitter.ts
+++ packages/typespec-python/src/emitter.ts
@@ -1,8 +1,8 @@
 import { EmitContext } from "@typespec/compiler";
-import { PythonEmitterOptions } from "./lib.js";
+import { PythonAzureEmitterOptions } from "./lib.js";
 import { $onEmit as httpClientPythonOnEmit } from "@typespec/http-client-python";
 
-export async function $onEmit(context: EmitContext<PythonEmitterOptions>) {
+export async function $onEmit(context: EmitContext<PythonAzureEmitterOptions>) {
     // set flavor to azure if not set for python azure emitter
     if (context.options.flavor === undefined) {
         context.options.flavor = "azure";
