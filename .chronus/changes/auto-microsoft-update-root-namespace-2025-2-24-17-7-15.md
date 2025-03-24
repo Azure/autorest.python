8bcbc6554040540829b3c4f2c1e501ed9b553dec
Yuchao Yan <yuchaoyan@microsoft.com>
2025-03-24 17:46:59 +0800

    always respect typespec namesapce even if package-name is set (#3057)
    * update dependency
    
    * update dependency
    
    * regen
    
    * update
    
    * update
    
    * udpate
    
    * update
    
    * update
    
    * Update dependencies (2025-03-21 03:15:15)
    
    * Regenerate for typespec-python (2025-03-21 03:19:35)
    
    * Update dependencies (2025-03-24 03:10:07)
    
    * update
    
    * add changelog
    
    ---------
    
    Co-authored-by: AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>

---
+++ .chronus/changes/auto-microsoft-update-root-namespace-2025-2-24-17-7-15.md
@@ -0,0 +1,8 @@
+---
+changeKind: feature
+packages:
+  - "@autorest/python"
+  - "@azure-tools/typespec-python"
+---
+
+Always respect namespace from TCGC
--- packages/typespec-python/test/azure/generated/routes/routes/operations/__init__.py
+++ packages/typespec-python/test/azure/generated/routes/routes/operations/__init__.py
@@ -12,8 +12,6 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._operations import PathParametersOperations  # type: ignore
-from ._operations import QueryParametersOperations  # type: ignore
 from ._operations import InInterfaceOperations  # type: ignore
 from ._operations import RoutesClientOperationsMixin  # type: ignore
 
@@ -22,8 +20,6 @@ from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "PathParametersOperations",
-    "QueryParametersOperations",
     "InInterfaceOperations",
     "RoutesClientOperationsMixin",
 ]
