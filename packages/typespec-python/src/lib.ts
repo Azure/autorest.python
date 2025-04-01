de30f5e20bb650e57afa4e5e6d6efc41548cfc7d
tadelesh <tadelesh.shi@live.cn>
2025-04-01 15:37:19 +0800

    change option

--- packages/typespec-python/src/lib.ts
+++ packages/typespec-python/src/lib.ts
@@ -1,8 +1,11 @@
-import { SdkContext, SdkServiceOperation } from "@azure-tools/typespec-client-generator-core";
+import { SdkContext, SdkServiceOperation, BrandedSdkEmitterOptions } from "@azure-tools/typespec-client-generator-core";
 import { createTypeSpecLibrary, JSONSchemaType } from "@typespec/compiler";
 import { PythonEmitterOptions, PythonEmitterOptionsSchema } from "@typespec/http-client-python";
 
 export interface PythonAzureEmitterOptions extends PythonEmitterOptions {
+    "examples-dir"?: string;
+    namespace?: string;
+
     "flavor"?: "azure";
     "models-mode"?: string;
     "generate-sample"?: boolean;
@@ -16,8 +19,11 @@ export interface PythonSdkContext<TServiceOperation extends SdkServiceOperation>
 
 const PythonAzureEmitterOptionsSchema: JSONSchemaType<PythonAzureEmitterOptions> = {
     type: "object",
-    additionalProperties: true,
+    additionalProperties: false,
     properties: {
+        ...BrandedSdkEmitterOptions["examples-dir"],
+        ...BrandedSdkEmitterOptions["namespace"],
+
         "flavor": {
             type: "string",
             nullable: true,
