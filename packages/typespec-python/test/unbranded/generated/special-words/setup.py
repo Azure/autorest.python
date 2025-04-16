a2acca8e132e67797009511d9e34deeac512df5e
iscai-msft <isabellavcai@gmail.com>
2025-04-16 11:01:48 -0400

    regen with setup.py changes

--- packages/typespec-python/test/unbranded/generated/special-words/setup.py
+++ packages/typespec-python/test/unbranded/generated/special-words/setup.py
@@ -51,7 +51,7 @@ setup(
     },
     install_requires=[
         "isodate>=0.6.1",
-        "corehttp[requests]>=1.0.0b6",
+        "corehttp[requests]",
         "typing-extensions>=4.6.0",
     ],
     python_requires=">=3.9",
