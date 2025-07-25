3746677fce3f2d4af5456beb4bddac892b5e456a
iscai-msft <isabellavcai@gmail.com>
2025-07-25 13:10:35 -0400

    add back patch of mixin oeprations

--- packages/typespec-python/test/unbranded/generated/authentication-api-key/authentication/apikey/aio/_operations/_patch.py
+++ packages/typespec-python/test/unbranded/generated/authentication-api-key/authentication/apikey/aio/_operations/_patch.py
@@ -6,7 +6,15 @@ Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python
 """
 from typing import List
 
-__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
+from ._operations import _ApiKeyClientOperationsMixin as Generated
+
+class _ApiKeyClientOperationsMixin(Generated):
+    
+    def patch_added_operation(self, **kwargs):
+        """This is a placeholder for the operation that was added in the patch."""
+        return True
+
+__all__: List[str] = ["_ApiKeyClientOperationsMixin"]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
