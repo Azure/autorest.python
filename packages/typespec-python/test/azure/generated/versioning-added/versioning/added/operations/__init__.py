48dc1a2abf8c5aa20e0c6caa8aa0dda097f437f8
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2025-07-28 22:29:07 +0000

    Regenerate for typespec-python (2025-07-28 22:29:07)

--- packages/typespec-python/test/azure/generated/versioning-added/versioning/added/operations/__init__.py
+++ packages/typespec-python/test/azure/generated/versioning-added/versioning/added/operations/__init__.py
@@ -13,6 +13,7 @@ if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
 from ._operations import InterfaceV2Operations  # type: ignore
+from ._operations import _AddedClientOperationsMixin  # type: ignore # pylint: disable=unused-import
 
 from ._patch import __all__ as _patch_all
 from ._patch import *
--- packages/typespec-python/test/unbranded/generated/server-versions-versioned/server/versions/versioned/_client.py
+++ packages/typespec-python/test/unbranded/generated/server-versions-versioned/server/versions/versioned/_client.py
@@ -8,7 +8,7 @@ from corehttp.rest import HttpRequest, HttpResponse
 from corehttp.runtime import PipelineClient, policies
 
 from ._configuration import VersionedClientConfiguration
-from ._operations._operations import _VersionedClientOperationsMixin
+from ._operations import _VersionedClientOperationsMixin
 from ._utils.serialization import Deserializer, Serializer
 
 
