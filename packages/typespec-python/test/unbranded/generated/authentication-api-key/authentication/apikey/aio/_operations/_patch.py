bf663a8d0a4a5573e9ad9459b985908a2b11ab84
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2025-07-29 15:06:31 -0400

    fix regression that ignored patched mixin operation groups (#3134)

--- packages/typespec-python/test/unbranded/generated/authentication-api-key/authentication/apikey/aio/_operations/_patch.py
+++ packages/typespec-python/test/unbranded/generated/authentication-api-key/authentication/apikey/aio/_operations/_patch.py
@@ -6,7 +6,25 @@ Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python
 """
 from typing import List
 
-__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
+from ._operations import _ApiKeyClientOperationsMixin as Generated
+
+
+class _ApiKeyClientOperationsMixin(Generated):
+
+    def patch_added_operation(self) -> bool:
+        """This is a placeholder for the operation that was added in the patch.
+
+        This method is a no-op and does not perform any action.
+
+        :return: Always returns True.
+        :rtype: bool
+        """
+        return True
+
+
+__all__: List[str] = [
+    "_ApiKeyClientOperationsMixin"
+]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
