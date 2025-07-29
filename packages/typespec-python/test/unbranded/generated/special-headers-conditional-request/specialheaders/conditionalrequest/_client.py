16426310fbf1e4a8e53fe7dc28d26870188fcacb
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2025-07-29 07:08:50 +0000

    Regenerate for typespec-python (2025-07-29 07:08:50)

--- packages/typespec-python/test/unbranded/generated/special-headers-conditional-request/specialheaders/conditionalrequest/_client.py
+++ packages/typespec-python/test/unbranded/generated/special-headers-conditional-request/specialheaders/conditionalrequest/_client.py
@@ -8,7 +8,7 @@ from corehttp.rest import HttpRequest, HttpResponse
 from corehttp.runtime import PipelineClient, policies
 
 from ._configuration import ConditionalRequestClientConfiguration
-from ._operations._operations import _ConditionalRequestClientOperationsMixin
+from ._operations import _ConditionalRequestClientOperationsMixin
 from ._utils.serialization import Deserializer, Serializer
 
 
