1069c742ff7a4b5d381781c793f748a739cb90f0
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2025-08-08 08:17:05 +0000

    Regenerate for typespec-python (2025-08-08 08:17:05)

--- packages/typespec-python/test/azure/generated/authentication-http-custom/authentication/http/custom/_operations/_operations.py
+++ packages/typespec-python/test/azure/generated/authentication-http-custom/authentication/http/custom/_operations/_operations.py
@@ -20,7 +20,6 @@ from azure.core.exceptions import (
 from azure.core.pipeline import PipelineResponse
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.core.tracing.decorator import distributed_trace
-from azure.core.utils import case_insensitive_dict
 
 from .. import models as _models
 from .._configuration import CustomClientConfiguration
@@ -43,17 +42,10 @@ def build_custom_valid_request(**kwargs: Any) -> HttpRequest:
 
 
 def build_custom_invalid_request(**kwargs: Any) -> HttpRequest:
-    _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
-
-    accept = _headers.pop("Accept", "application/json")
-
     # Construct URL
     _url = "/authentication/http/custom/invalid"
 
-    # Construct headers
-    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
-
-    return HttpRequest(method="GET", url=_url, headers=_headers, **kwargs)
+    return HttpRequest(method="GET", url=_url, **kwargs)
 
 
 class _CustomClientOperationsMixin(
