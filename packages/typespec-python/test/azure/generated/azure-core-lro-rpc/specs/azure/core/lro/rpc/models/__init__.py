29fc9665ba8dd9310206747faacd4fce966a8726
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2025-08-21 08:08:42 +0000

    Regenerate for typespec-python (2025-08-21 08:08:42)

--- packages/typespec-python/test/azure/generated/azure-core-lro-rpc/specs/azure/core/lro/rpc/models/__init__.py
+++ packages/typespec-python/test/azure/generated/azure-core-lro-rpc/specs/azure/core/lro/rpc/models/__init__.py
@@ -16,11 +16,6 @@ if TYPE_CHECKING:
 from ._models import (  # type: ignore
     GenerationOptions,
     GenerationResult,
-    ResourceOperationStatusGenerationResponseGenerationResultError,
-)
-
-from ._enums import (  # type: ignore
-    OperationState,
 )
 from ._patch import __all__ as _patch_all
 from ._patch import *
@@ -29,8 +24,6 @@ from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     "GenerationOptions",
     "GenerationResult",
-    "ResourceOperationStatusGenerationResponseGenerationResultError",
-    "OperationState",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
 _patch_sdk()
