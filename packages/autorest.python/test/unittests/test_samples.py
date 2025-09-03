abd4e37f84c1d5eff1677adc9f9610ae2f7a5b24
Yuchao Yan <yuchaoyan@microsoft.com>
2025-09-03 14:52:46 +0800

    add test case

--- packages/autorest.python/test/unittests/test_samples.py
+++ packages/autorest.python/test/unittests/test_samples.py
@@ -78,3 +78,8 @@ def test_package_mode_for_azure_mgmt_pyproject():
     packaging = pyproject_data.get("packaging", {})
     assert packaging.get("is_stable") is False, "is_stable shall be kept in pyproject.toml"
     assert packaging.get("is_arm") is True, "is_arm shall be kept in pyproject.toml"
+
+    dependencies = packaging.get("project", {}).get("dependencies", [])
+    assert any(
+        dep.startswith("azure-mgmt-core") for dep in dependencies
+    ), "azure-mgmt-core dependencies shall be kept in pyproject.toml"
