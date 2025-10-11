beb4487713af73c58e067d9200644c20583f73da
Yuchao Yan <yuchaoyan@microsoft.com>
2025-10-11 09:50:31 +0000

    fix test case

--- packages/typespec-python/test/azure/mock_api_tests/test_clear_output_folder.py
+++ packages/typespec-python/test/azure/mock_api_tests/test_clear_output_folder.py
@@ -9,11 +9,14 @@ GENERATED_PATH = Path(__file__).parent.parent.resolve() / "generated"
 
 
 def test_clear_output_folder():
-    assert GENERATED_PATH.exists(), "Generated path should exist"
-    assert not (
-        GENERATED_PATH / "authentication-api-key/authentication/apiKey/_operations/to_be_deleted.py"
-    ).exists(), "File to_be_deleted.py should be deleted after regeneration"
+    folder = GENERATED_PATH / "authentication-api-key/authentication/apikey/_operations"
+    assert folder.exists(), "Operations folder should exist"
+    assert not (folder / "to_be_deleted.py").exists(), "File to_be_deleted.py should be deleted after regeneration"
 
+    assert (GENERATED_PATH / "generation-subdir/generated_tests").exists()
     assert not (GENERATED_PATH / "generation-subdir/generated_tests/to_be_deleted.py").exists()
+
+    assert (GENERATED_PATH / "generation-subdir/generation/subdir/_generated").exists()
     assert not (GENERATED_PATH / "generation-subdir/generation/subdir/_generated/to_be_deleted.py").exists()
+
     assert (GENERATED_PATH / "generation-subdir/generation/subdir/to_be_kept.py").exists()
