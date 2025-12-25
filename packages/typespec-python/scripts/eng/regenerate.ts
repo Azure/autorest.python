fcc532662ffc194326836fe31ab50a2e34abcdad
Yuchao Yan <yuchaoyan@microsoft.com>
2025-12-25 11:28:13 +0800

    [python] support multi-service scenario (#3291)
    * add test case
    
    * Update dependencies
    
    * Add changelog
    
    * Update dependencies (2025-12-23 08:48:51)
    
    * Regenerate for typespec-python (2025-12-23 08:54:12)
    
    * update
    
    * update
    
    * Update dependencies
    
    * Add changelog
    
    * Update dependencies (2025-12-24 03:04:08)
    
    * Regenerate for typespec-python (2025-12-24 03:09:36)
    
    * Update dependencies
    
    * Update dependencies (2025-12-24 09:01:58)
    
    ---------
    
    Co-authored-by: AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>

--- packages/typespec-python/scripts/eng/regenerate.ts
+++ packages/typespec-python/scripts/eng/regenerate.ts
@@ -131,6 +131,9 @@ const AZURE_EMITTER_OPTIONS: Record<string, Record<string, string> | Record<stri
     "special-words": {
         namespace: "specialwords",
     },
+    "service/multi-service": {
+        namespace: "service.multiservice",
+    },
 };
 
 const EMITTER_OPTIONS: Record<string, Record<string, string> | Record<string, string>[]> = {
