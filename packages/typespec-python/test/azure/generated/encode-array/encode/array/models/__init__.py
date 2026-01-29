065b1643850f7b649b83fc2a7761ae04b732dbb8
AutoPrFromHttpClientPython <AutoPrFromHttpClientPython>
2026-01-29 05:12:00 +0000

    Regenerate for typespec-python (2026-01-29 05:12:00)

--- packages/typespec-python/test/azure/generated/encode-array/encode/array/models/__init__.py
+++ packages/typespec-python/test/azure/generated/encode-array/encode/array/models/__init__.py
@@ -15,22 +15,9 @@ if TYPE_CHECKING:
 
 from ._models import (  # type: ignore
     CommaDelimitedArrayProperty,
-    CommaDelimitedEnumArrayProperty,
-    CommaDelimitedExtensibleEnumArrayProperty,
     NewlineDelimitedArrayProperty,
-    NewlineDelimitedEnumArrayProperty,
-    NewlineDelimitedExtensibleEnumArrayProperty,
     PipeDelimitedArrayProperty,
-    PipeDelimitedEnumArrayProperty,
-    PipeDelimitedExtensibleEnumArrayProperty,
     SpaceDelimitedArrayProperty,
-    SpaceDelimitedEnumArrayProperty,
-    SpaceDelimitedExtensibleEnumArrayProperty,
-)
-
-from ._enums import (  # type: ignore
-    Colors,
-    ColorsExtensibleEnum,
 )
 from ._patch import __all__ as _patch_all
 from ._patch import *
@@ -38,19 +25,9 @@ from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
     "CommaDelimitedArrayProperty",
-    "CommaDelimitedEnumArrayProperty",
-    "CommaDelimitedExtensibleEnumArrayProperty",
     "NewlineDelimitedArrayProperty",
-    "NewlineDelimitedEnumArrayProperty",
-    "NewlineDelimitedExtensibleEnumArrayProperty",
     "PipeDelimitedArrayProperty",
-    "PipeDelimitedEnumArrayProperty",
-    "PipeDelimitedExtensibleEnumArrayProperty",
     "SpaceDelimitedArrayProperty",
-    "SpaceDelimitedEnumArrayProperty",
-    "SpaceDelimitedExtensibleEnumArrayProperty",
-    "Colors",
-    "ColorsExtensibleEnum",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
 _patch_sdk()
--- packages/typespec-python/test/unbranded/generated/encode-array/encode/array/models/__init__.py
+++ packages/typespec-python/test/unbranded/generated/encode-array/encode/array/models/__init__.py
@@ -9,22 +9,9 @@ if TYPE_CHECKING:
 
 from ._models import (  # type: ignore
     CommaDelimitedArrayProperty,
-    CommaDelimitedEnumArrayProperty,
-    CommaDelimitedExtensibleEnumArrayProperty,
     NewlineDelimitedArrayProperty,
-    NewlineDelimitedEnumArrayProperty,
-    NewlineDelimitedExtensibleEnumArrayProperty,
     PipeDelimitedArrayProperty,
-    PipeDelimitedEnumArrayProperty,
-    PipeDelimitedExtensibleEnumArrayProperty,
     SpaceDelimitedArrayProperty,
-    SpaceDelimitedEnumArrayProperty,
-    SpaceDelimitedExtensibleEnumArrayProperty,
-)
-
-from ._enums import (  # type: ignore
-    Colors,
-    ColorsExtensibleEnum,
 )
 from ._patch import __all__ as _patch_all
 from ._patch import *
@@ -32,19 +19,9 @@ from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
     "CommaDelimitedArrayProperty",
-    "CommaDelimitedEnumArrayProperty",
-    "CommaDelimitedExtensibleEnumArrayProperty",
     "NewlineDelimitedArrayProperty",
-    "NewlineDelimitedEnumArrayProperty",
-    "NewlineDelimitedExtensibleEnumArrayProperty",
     "PipeDelimitedArrayProperty",
-    "PipeDelimitedEnumArrayProperty",
-    "PipeDelimitedExtensibleEnumArrayProperty",
     "SpaceDelimitedArrayProperty",
-    "SpaceDelimitedEnumArrayProperty",
-    "SpaceDelimitedExtensibleEnumArrayProperty",
-    "Colors",
-    "ColorsExtensibleEnum",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
 _patch_sdk()
